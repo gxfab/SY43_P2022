227c3b287bda04fbd5e104a994670f8ead92e344
Brice Van Aken <brice.van-aken@utbm.fr>
2022-04-20 12:16:41 +0200

    First fragments created, app menu appearing right now. We need to add button listener in order to navigate between fragments.

--- app/src/main/java/com/example/sy43_p2022/MainActivity.kt
+++ app/src/main/java/com/example/sy43_p2022/MainActivity.kt
@@ -1,58 +1,18 @@
 package com.example.sy43_p2022
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
-import com.example.sy43_p2022.databinding.ActivityMainBinding
+import com.example.sy43_p2022.fragments.HomeFragment
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        setSupportActionBar(binding.toolbar)
-
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
+        //On injecte le HomeFragment dans notre fragment_container de activity_main
+        val transaction = supportFragmentManager.beginTransaction() //necessaire pour manipuler les fragments
+        transaction.replace(R.id.fragment_container, HomeFragment()) //remplacement du fragment (en attente)
+        transaction.addToBackStack(null) //pas de retour
+        transaction.commit() //on effectue le remplacement
     }
 }
---
+++ app/src/main/java/com/example/sy43_p2022/fragments/HomeFragment.kt
@@ -0,0 +1,14 @@
+package com.example.sy43_p2022.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.sy43_p2022.R
+
+class HomeFragment:Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater?.inflate(R.layout.fragment_home, container, false)
+    }
+}
