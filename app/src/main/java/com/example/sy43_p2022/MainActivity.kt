1d1606cd4e11e24727405abbf72e6835f8c3ee20
Brice Van Aken <brice.van-aken@utbm.fr>
2022-04-20 22:41:18 +0200

    return button working

--- app/src/main/java/com/example/sy43_p2022/MainActivity.kt
+++ app/src/main/java/com/example/sy43_p2022/MainActivity.kt
@@ -1,11 +1,8 @@
 package com.example.sy43_p2022
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.sy43_p2022.fragments.HomeFragment
-import com.example.sy43_p2022.fragments.SpendingsFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,7 +14,6 @@ class MainActivity : AppCompatActivity() {
         transaction.replace(R.id.fragment_container, HomeFragment()) //remplacement du fragment (en attente)
         transaction.addToBackStack(null) //pas de retour
         transaction.commit() //on effectue le remplacement
-
 
     }
 }
--- app/src/main/java/com/example/sy43_p2022/fragments/SpendingsFragment.kt
+++ app/src/main/java/com/example/sy43_p2022/fragments/SpendingsFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.example.sy43_p2022.R
 import com.example.sy43_p2022.adapter.ButtonAdapter
@@ -17,6 +20,15 @@ class SpendingsFragment:Fragment() {
         //ici, on va récupérer le recyclerview pour afficher la lsite des boutons
         val verticalRecyclerView = view.findViewById<RecyclerView>(R.id.vertical_recycler_view)
         verticalRecyclerView.adapter = ButtonAdapter()
+
+        //Ici, on implémente le bouton du retour
+        val clickReturn = view.findViewById<ImageView>(R.id.fragment_spending_return)
+        clickReturn.setOnClickListener {
+            val transaction = (activity as FragmentActivity).supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.fragment_container, HomeFragment())
+            transaction.addToBackStack(null)
+            transaction.commit()
+        }
         return view
     }
 }
