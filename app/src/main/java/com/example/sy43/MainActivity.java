5e4f4f954480d06497f00bfb2561aa10f699711a
ercan <sarpcanercan@gmail.com>
2022-04-29 14:01:58 +0200

    Added category

--- app/src/main/java/com/example/sy43/MainActivity.java
+++ app/src/main/java/com/example/sy43/MainActivity.java
@@ -5,10 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.category);
+
     }
 }
