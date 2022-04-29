3e6eb30806a96cc0dff600cb94f753389025aad7
ercan <sarpcanercan@gmail.com>
2022-04-29 15:09:24 +0200

    New activity

---
+++ app/src/main/java/com/example/sy43/Category.java
@@ -0,0 +1,15 @@
+package com.example.sy43;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Category extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setTitle("Category page");
+        setContentView(R.layout.activity_category);
+    }
+}
--- app/src/main/java/com/example/sy43/MainActivity.java
+++ app/src/main/java/com/example/sy43/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.sy43;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.category);
+        setContentView(R.layout.activity_main);
+        final Button button = findViewById(R.id.category);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "It works?", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(v.getContext(), Category.class);
+                v.getContext().startActivity(intent);
+            }
 
+        });
     }
 }
