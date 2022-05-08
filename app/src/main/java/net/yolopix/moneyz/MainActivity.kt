3a72717b0f74752f926c131a248501b6e55a2468
elios-cama <elios.cama@gmail.com>
2022-05-08 11:07:17 +0200

    added some widgets for the homepage

--- app/src/main/java/net/yolopix/moneyz/MainActivity.kt
+++ app/src/main/java/net/yolopix/moneyz/MainActivity.kt
@@ -2,10 +2,13 @@ package net.yolopix.moneyz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var DepensesRV : RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        DepensesRV = findViewById(R.id.RV_Spending)
     }
 }
