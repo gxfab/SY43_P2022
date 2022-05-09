88d15d0ba79603ff3845a6c662e42a3c2d180f6e
CROUTS DE PAILLE Benjamin <bencdep@gmail.com>
2022-05-09 10:49:13 +0200

    Moved Foreign Key from Categories to SubCategories because of a mistake in conception

--- Sucelloz/app/src/main/java/com/sucelloztm/sucelloz/models/Categories.java
+++ Sucelloz/app/src/main/java/com/sucelloztm/sucelloz/models/Categories.java
@@ -1,2 +1,46 @@
-package com.sucelloztm.sucelloz.models;public class Categories {
+package com.sucelloztm.sucelloz.models;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+import androidx.room.ForeignKey;
+import java.lang.String;
+
+@Entity(tableName = "categories")
+public class Categories {
+
+    @PrimaryKey
+    private long id;
+
+    @ColumnInfo(name="name")
+    private String name;
+
+    @ColumnInfo(name="read_only")
+    private boolean readOnly;
+
+    //CONSTRUCTOR
+    public Categories(){}
+    public Categories(String name,boolean readOnly,long subCategoriesId){
+        this.name=name;
+        this.readOnly=readOnly;
+    }
+    //GETTER
+    public long getId(){
+        return id;
+    }
+    public String getName(){
+        return name;
+    }
+    public boolean getReadOnly(){
+        return readOnly;
+    }
+    //SETTER
+    public void setId(long id){
+        this.id=id;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+    public void setReadOnly(boolean readOnly){
+        this.readOnly=readOnly;
+    }
 }
--- Sucelloz/app/src/main/java/com/sucelloztm/sucelloz/models/SubCategories.java
+++ Sucelloz/app/src/main/java/com/sucelloztm/sucelloz/models/SubCategories.java
@@ -1,2 +1,49 @@
-package com.sucelloztm.sucelloz.models;public class SubCategories {
+package com.sucelloztm.sucelloz.models;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.PrimaryKey;
+
+import java.lang.String;
+
+@Entity(tableName = "sub_categories",
+        foreignKeys = {@ForeignKey(entity = Categories.class,
+                parentColumns = "id",
+                childColumns = "categories_id",
+                onDelete = ForeignKey.CASCADE)}
+)
+public class SubCategories {
+
+    @PrimaryKey
+    private long id;
+
+    @ColumnInfo(name = "name")
+    private String name;
+
+    @ColumnInfo(name="categories_id")
+    private long categoriesId;
+
+    //CONSTRUCTOR
+    public SubCategories(){}
+    public SubCategories(String name,long categoriesId){
+        this.name=name;
+        this.categoriesId=categoriesId;
+    }
+    //GETTER
+    public long getId(){
+        return id;
+    }
+    public String getName(){
+        return name;
+    }
+    public long getCategoriesId(){ return categoriesId; }
+
+    //SETTER
+    public void setId(long id){
+        this.id=id;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+    public void setCategoriesId(long categoriesId){ this.categoriesId=categoriesId; }
 }
