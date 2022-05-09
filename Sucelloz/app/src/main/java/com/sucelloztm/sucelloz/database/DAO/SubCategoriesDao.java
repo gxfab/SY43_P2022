3f7cf5a7e4c3823eefd7c77bb3f393dc11720454
CROUTS DE PAILLE Benjamin <bencdep@gmail.com>
2022-05-09 10:50:11 +0200

    Moved Foreign Key from Categories to SubCategories because of a mistake in conception

--- Sucelloz/app/src/main/java/com/sucelloztm/sucelloz/database/DAO/SubCategoriesDao.java
+++ Sucelloz/app/src/main/java/com/sucelloztm/sucelloz/database/DAO/SubCategoriesDao.java
@@ -1,2 +1,30 @@
-package com.sucelloztm.sucelloz.database.DAO;public interface SubCategoriesDao {
+package com.sucelloztm.sucelloz.database.DAO;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Update;
+
+import com.sucelloztm.sucelloz.models.SubCategories;
+
+@Dao
+public interface SubCategoriesDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    public void insertSubCategory(SubCategories subCategory);
+
+    @Insert
+    public void insertSubCategories(SubCategories... subCategories);
+
+    @Update
+    public void updateSubCategory(SubCategories subCategory);
+
+    @Update
+    public void updateSubCategories(SubCategories... subCategories);
+
+    @Delete
+    public void deleteSubCategory(SubCategories subCategory);
+
+    @Delete
+    public void deleteSubCategories(SubCategories... subCategories);
 }
