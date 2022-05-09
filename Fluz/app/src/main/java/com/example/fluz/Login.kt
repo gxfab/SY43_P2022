ae439cbe163053cd0a0eac16d6a95270f7178eb0
Maureen-gdd <maureen.grandidier@gmail.com>
2022-05-09 10:09:54 +0200

    [Kotlin Class] Creation of the page "LoginOrRegister"
    Push the button register, login or continue without login will redirect you on another page

--- Fluz/app/src/main/java/com/example/fluz/Login.kt
+++ Fluz/app/src/main/java/com/example/fluz/Login.kt
@@ -11,7 +11,7 @@ import com.example.fluz.databinding.FragmentSecondBinding
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class SecondFragment : Fragment() {
+class Login : Fragment() {
 
     private var _binding: FragmentSecondBinding? = null
 
--- Fluz/app/src/main/java/com/example/fluz/LoginOrRegister.kt
+++ Fluz/app/src/main/java/com/example/fluz/LoginOrRegister.kt
@@ -6,14 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import com.example.fluz.databinding.FragmentFirstBinding
+import com.example.fluz.databinding.LoginOrRegisterBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class LoginOrRegister : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
+    private var _binding: LoginOrRegisterBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -24,7 +24,7 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        _binding = LoginOrRegisterBinding.inflate(inflater, container, false)
         return binding.root
 
     }
@@ -32,7 +32,15 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
+        binding.btnLogin.setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+
+        binding.btnRegister.setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+
+        binding.txtWithoutLogin.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
     }
