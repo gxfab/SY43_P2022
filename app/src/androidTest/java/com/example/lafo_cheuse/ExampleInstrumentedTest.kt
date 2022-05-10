6e99d32aec1787772736644675d968e74850ddbb
David Goblot <david.goblot26@gmail.com>
2022-05-10 11:54:14 +0200

    Template change

--- app/src/androidTest/java/com/example/lafo_cheuse/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/lafo_cheuse/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.lafo_cheuse", appContext.packageName)
+        assertEquals("com.example.test", appContext.packageName)
     }
 }
---
+++ app/src/main/java/com/example/lafo_cheuse/DisplayMessageActivity.kt
@@ -0,0 +1,18 @@
+package com.example.lafo_cheuse
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class DisplayMessageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_display_message)
+
+        val message = intent.getStringExtra(EXTRA_MESSAGE)
+
+        val textView = findViewById<TextView>(R.id.textView).apply {
+            text = message
+        }
+    }
+}
