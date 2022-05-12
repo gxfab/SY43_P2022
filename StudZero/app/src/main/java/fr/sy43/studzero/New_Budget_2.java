17b84fb8e31fc3400eeefa47b94c3029cc5567b4
Simje <59029949+simje71380@users.noreply.github.com>
2022-05-12 16:08:04 +0200

    création New_Budget_2 manque com avec DB - création de CustomAdapter et my_row.xml

--- StudZero/app/src/main/java/fr/sy43/studzero/New_Budget_2.java
+++ StudZero/app/src/main/java/fr/sy43/studzero/New_Budget_2.java
@@ -1,13 +1,28 @@
 package fr.sy43.studzero;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
+import java.util.ArrayList;
+
+import fr.sy43.studzero.activities.CustomAdapter;
+
+//récupération des catégories déjà existante : CTRL+F "récup data"
+//remplir la BD : CTRL+F : "remplir la BD"
 public class New_Budget_2 extends AppCompatActivity {
+
+    private CustomAdapter customAdapter;
+    private ArrayList<String> category_name;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +32,59 @@ public class New_Budget_2 extends AppCompatActivity {
         //on viens forcément de l'écran settings
         getSupportActionBar().setTitle("Setup New Budget");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //init
+        category_name = new ArrayList<>();
+        recyclerView = findViewById(R.id.recyclerView);
+        GetData();
+        customAdapter = new CustomAdapter(this.getApplicationContext(), category_name);
+        recyclerView.setAdapter(customAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this.getApplicationContext()));
+
+
+        Button button = (Button) findViewById(R.id.NewBudget2_ConfirmButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //Ajouter les catégories que l'on garde à la BD
+                SetSelectedCat(category_name, customAdapter.state);
+                finish();
+                //on viens forcement de settings pur cette page
+                Intent intent = new Intent(getApplicationContext(), New_Budget_3.class);
+                intent.putExtra("caller", "Settings"); //permet à la nouvelle activity de connaitre son lanceur
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    //récupérer les datas de la DB courrante
+    private void GetData() {
+        // récup data
+        category_name.add("cat 1");
+        category_name.add("cat 2");
+        category_name.add("cat 3");
+        category_name.add("cat 4");
+        category_name.add("cat 5");
+        category_name.add("cat 6");
+        category_name.add("cat 7");
+        category_name.add("cat 8");
+        category_name.add("cat 9");
+        category_name.add("cat 10");
+        category_name.add("cat 11");
+        category_name.add("cat 12");
+    }
+
+    private  void SetSelectedCat(ArrayList category_name,ArrayList state){
+        //remplir la BD
+        for(int i = 0; i < category_name.size(); ++i){
+            if(state.get(i).equals(1)){//on garde cette catégorie
+                Log.i("add cat", String.valueOf(category_name.get(i)));
+            }
+        }
     }
 
 
-    //permet le retour vers la page settings sur l'appuis de la back arrow
+    //permet le retour vers la page précédente
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
---
+++ StudZero/app/src/main/java/fr/sy43/studzero/activities/CustomAdapter.java
@@ -0,0 +1,107 @@
+package fr.sy43.studzero.activities;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
+import android.widget.LinearLayout;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.widget.SwitchCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+import fr.sy43.studzero.New_Budget_2;
+import fr.sy43.studzero.R;
+
+public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHolder> {
+
+    private Context context;
+    private ArrayList category_name;
+    public ArrayList<Integer> state = new ArrayList<Integer>();
+
+    public CustomAdapter(Context context, ArrayList category_name){
+        this.category_name = category_name;
+        this.context = context;
+        for(int i = 0; i < category_name.size(); i++){
+            state.add(i, 1);//set a 1 par défaut
+        }
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.new_budget_2_my_row, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.category_name.setText(String.valueOf(this.category_name.get(position)));
+        holder.button.setChecked(true);
+
+        holder.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(holder.button.isChecked()){
+                    state.set(holder.getAdapterPosition(), 1);
+                }
+                else{
+                    state.set(holder.getAdapterPosition(), 0);
+                }
+                Log.i("OnClick", String.valueOf(category_name.get(holder.getAdapterPosition())));
+                Log.i("state", String.valueOf(state.get(holder.getAdapterPosition())));
+            }
+        });
+        holder.mainLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //change le state du button / switch
+                if(holder.button.isChecked()){
+                    holder.button.setChecked(false);
+                    state.set(holder.getAdapterPosition(), 0);
+                }
+                else{
+                    holder.button.setChecked(true);
+                    state.set(holder.getAdapterPosition(), 1);
+                }
+                Log.i("OnClick", String.valueOf(category_name.get(holder.getAdapterPosition())));
+                Log.i("state", String.valueOf(state.get(holder.getAdapterPosition())));
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return category_name.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+        TextView category_name;
+        CompoundButton button;
+        LinearLayout mainLayout;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            category_name = itemView.findViewById(R.id.cat_name_txt);
+            button = itemView.findViewById(R.id.switch1);
+            mainLayout = itemView.findViewById(R.id.mainLayout);
+        }
+    }
+}
