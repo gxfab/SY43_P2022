0927a556028d09096492fce883e2a1e06fb135c8
lsacienne <alexandre.viala@outlook.com>
2022-05-14 13:18:52 +0200

    Add of pie chart on the main activity

--- app/src/main/java/com/example/lafo_cheuse/MainActivity.kt
+++ app/src/main/java/com/example/lafo_cheuse/MainActivity.kt
@@ -1,10 +1,18 @@
 package com.example.lafo_cheuse
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.components.Description
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.utils.ColorTemplate
+
 
 const val EXTRA_MESSAGE = "com.exemple.test.MESSAGE"
 
@@ -12,6 +20,27 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val chart = findViewById<PieChart>(R.id.piechart)
+        val entries = ArrayList<PieEntry>()
+
+        for (i in 0..3) {
+            entries.add(PieEntry((Math.random() * 70 + 30).toFloat(), "Quarter " + (i + 1)))
+        }
+        val dataSet = PieDataSet(entries, "")
+
+        dataSet.colors = ColorTemplate.PASTEL_COLORS.toList()
+        dataSet.valueTextColor = Color.WHITE
+        dataSet.valueTextSize = 12f
+
+        val data = PieData(dataSet)
+        chart.data = data
+        val description = Description()
+        description.textColor = Color.WHITE
+        description.text = "Bootleg graph"
+        chart.description = description
+        chart.animateX(2000)
+
     }
     fun sendMessage(view: View) {
         val editText = findViewById<EditText>(R.id.editTextTextPersonName)
