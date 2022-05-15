60a6b3c8e6863c32f3325a5f22d08b1f670cf1b3
lsacienne <alexandre.viala@outlook.com>
2022-05-15 12:31:01 +0200

    Add of TODO in the Frequency enum class.

--- app/src/main/java/com/example/lafo_cheuse/models/Frequency.kt
+++ app/src/main/java/com/example/lafo_cheuse/models/Frequency.kt
@@ -3,5 +3,6 @@ package com.example.lafo_cheuse.models
 enum class Frequency(val frequency: Int) {
     OUNCE_A_MONTH(1),
     OUNCE_A_WEEK(4),
+    /* TODO : adapt to the current month */
     OUNCE_A_DAY(31)
 }
