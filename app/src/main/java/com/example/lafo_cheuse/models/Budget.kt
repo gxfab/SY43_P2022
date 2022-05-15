207c903272db2e1e99fb5f219e01b0a0dc4246a0
lsacienne <alexandre.viala@outlook.com>
2022-05-15 12:28:39 +0200

    Add of name to the different columns of Budget table.

--- app/src/main/java/com/example/lafo_cheuse/models/Budget.kt
+++ app/src/main/java/com/example/lafo_cheuse/models/Budget.kt
@@ -1,5 +1,6 @@
 package com.example.lafo_cheuse.models
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
@@ -7,9 +8,13 @@ import androidx.room.PrimaryKey
 class Budget {
 
     @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "budgetId")
     var budgetId: Long = 0
 
+    @ColumnInfo(name = "category")
     var budgetCategory: String = ""
+    @ColumnInfo(name = "moneyChange")
     var moneyChange: MoneyChange = MoneyChange()
+    @ColumnInfo(name = "duration")
     var duration: Duration = Duration.MONTH
 }
