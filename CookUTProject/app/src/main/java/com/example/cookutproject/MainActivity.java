ffcfbc8e44b06d04c66627d24aa14500d4648f6f
Sapoil <guillaume@fantou.eu>
2022-05-16 16:11:06 +0200

    Premiers pas de l'application

--- CookUTProject/app/src/main/java/com/example/cookutproject/MainActivity.java
+++ CookUTProject/app/src/main/java/com/example/cookutproject/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.cookutproject;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
         setContentView(R.layout.activity_main);
     }
 }
