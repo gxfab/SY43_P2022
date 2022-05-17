742ee1151bfb07bbfb79fb39f2a744cae5335c0d
lsacienne <alexandre.viala@outlook.com>
2022-05-17 18:07:18 +0200

    :card_file_box: Test of initialization of database. We must use a coroutine inside another thread in this way. Else, the application will bug.

--- app/src/main/java/com/example/lafo_cheuse/MainActivity.kt
+++ app/src/main/java/com/example/lafo_cheuse/MainActivity.kt
@@ -6,41 +6,61 @@ import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.room.Room
+import com.example.lafo_cheuse.database.LafoCheuseDatabase
+import com.example.lafo_cheuse.models.Category
 import com.github.mikephil.charting.charts.PieChart
 import com.github.mikephil.charting.components.Description
 import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
 import com.github.mikephil.charting.data.PieEntry
 import com.github.mikephil.charting.utils.ColorTemplate
+import kotlinx.coroutines.*
 
 
 const val EXTRA_MESSAGE = "com.exemple.test.MESSAGE"
 
+@DelicateCoroutinesApi
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val chart = findViewById<PieChart>(R.id.piechart)
-        val entries = ArrayList<PieEntry>()
+        GlobalScope.launch(Dispatchers.Main) {
 
-        for (i in 0..3) {
-            entries.add(PieEntry((Math.random() * 70 + 30).toFloat(), "Quarter " + (i + 1)))
+            val db = Room.databaseBuilder(
+                applicationContext,
+                LafoCheuseDatabase::class.java, "BDD Lafo-Cheuse"
+            ).build()
+            launch(Dispatchers.IO) {
+                val catDao = db.categoryDao()
+                catDao!!.createCategory(Category("Courses", "\uD83D\uDED2"))
+                catDao.createCategory(Category("Bourses", "\uD83D\uDCB0"))
+            }
+
+
+            val chart = findViewById<PieChart>(R.id.piechart)
+
+            val entries = ArrayList<PieEntry>()
+
+            for (i in 0..3) {
+                entries.add(PieEntry((Math.random() * 70 + 30).toFloat(), "Quarter " + (i + 1)))
+            }
+            val dataSet = PieDataSet(entries, "")
+
+            dataSet.colors = ColorTemplate.PASTEL_COLORS.toList()
+            dataSet.valueTextColor = Color.WHITE
+            dataSet.valueTextSize = 12f
+
+            val data = PieData(dataSet)
+
+            chart.data = data
+            val description = Description()
+            description.textColor = Color.RED
+            description.text = "Bootleg graph"
+            chart.description = description
+            chart.animateX(2000)
         }
-        val dataSet = PieDataSet(entries, "")
-
-        dataSet.colors = ColorTemplate.PASTEL_COLORS.toList()
-        dataSet.valueTextColor = Color.WHITE
-        dataSet.valueTextSize = 12f
-
-        val data = PieData(dataSet)
-        chart.data = data
-        val description = Description()
-        description.textColor = Color.RED
-        description.text = "Bootleg graph"
-        chart.description = description
-        chart.animateX(2000)
-
     }
     fun sendMessage(view: View) {
         val editText = findViewById<EditText>(R.id.editTextTextPersonName)
@@ -50,4 +70,5 @@ class MainActivity : AppCompatActivity() {
         }
         startActivity(intent)
     }
+
 }
