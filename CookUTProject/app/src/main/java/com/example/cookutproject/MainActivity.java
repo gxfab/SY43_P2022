3929ac56af42f77b679fa27f4049764b16c32f2e
Sapoil <guillaume@fantou.eu>
2022-05-17 14:48:55 +0200

    changements pratiques
    Mise en place de couleurs de bases, ajout d'une activity,
    graphismes de bases appliqué sur toute l'application

--- CookUTProject/app/src/main/java/com/example/cookutproject/MainActivity.java
+++ CookUTProject/app/src/main/java/com/example/cookutproject/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.cookutproject;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -11,8 +13,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.hide();
         setContentView(R.layout.activity_main);
+    }
+
+    public void changeActivity(View view){
+        Intent intent = new Intent(this, Saisie.class);
+        startActivity(intent);
     }
 }
---
+++ CookUTProject/app/src/main/java/com/example/cookutproject/Saisie.java
@@ -0,0 +1,14 @@
+package com.example.cookutproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Saisie extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saisie);
+    }
+}
