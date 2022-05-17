90d9e68f90a20525d3ecb5312eafb5c3cc7a1072
Jnius0 <rjnius@gmail.com>
2022-05-17 11:52:36 +0200

    finished établissement-list
    refactored with ng functions and services

---
+++ Sucelloz/app/src/main/java/com/sucelloztm/sucelloz/CustomAdapter.java
@@ -0,0 +1,69 @@
+package com.sucelloztm.sucelloz;
+
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder> {
+
+    private String[] localDataSet;
+
+    /**
+     * Provide a reference to the type of views that you are using
+     * (custom ViewHolder).
+     */
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private final TextView textView;
+
+        public ViewHolder(View view) {
+            super(view);
+            // Define click listener for the ViewHolder's View
+
+            textView = (TextView) view.findViewById(R.id.textView);
+        }
+
+        public TextView getTextView() {
+            return textView;
+        }
+    }
+
+    /**
+     * Initialize the dataset of the Adapter.
+     *
+     * @param dataSet String[] containing the data to populate views to be used
+     * by RecyclerView.
+     */
+    public CustomAdapter(String[] dataSet) {
+        localDataSet = dataSet;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        // Create a new view, which defines the UI of the list item
+        View view = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.text_row_item, viewGroup, false);
+
+        return new ViewHolder(view);
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, final int position) {
+
+        // Get element from your dataset at this position and replace the
+        // contents of the view with that element
+        viewHolder.getTextView().setText(localDataSet[position]);
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return localDataSet.length;
+    }
+}
+
--- Sucelloz/app/src/main/java/com/sucelloztm/sucelloz/MainActivity.java
+++ Sucelloz/app/src/main/java/com/sucelloztm/sucelloz/MainActivity.java
@@ -2,6 +2,8 @@ package com.sucelloztm.sucelloz;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -10,8 +12,15 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    List<String> toDoList;
+    ArrayAdapter<String> arrayAdapter;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +30,9 @@ public class MainActivity extends AppCompatActivity {
         //launch the nav bar
         Intent intent = new Intent(this, Nav.class);
         startActivity(intent);
+
+        toDoList=new ArrayList<>();
+        arrayAdapter=new ArrayAdapter<>(this,R.layout.text_row_item,toDoList);
+        listView=
     }
 }
