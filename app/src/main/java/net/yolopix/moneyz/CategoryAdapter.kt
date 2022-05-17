9f5827f365d6347d9f09dadc987b5e1763382245
elios-cama <elios.cama@gmail.com>
2022-05-17 13:05:28 +0200

    started item view

---
+++ app/src/main/java/net/yolopix/moneyz/CategoryAdapter.kt
@@ -0,0 +1,40 @@
+package net.yolopix.moneyz
+
+
+import net.yolopix.moneyz.model.database.Category
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import net.yolopix.moneyz.model.database.Account
+/*
+class CategoryAdapter(private val accountList: List<Category>):
+
+    RecyclerView.Adapter<CategoryAdapter.CategoryViewHolder>(){
+
+
+        class CategoryViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+            var textViewCategory : TextView? = itemView.findViewById(R.id.idTVNote)
+        }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryAdapter.CategoryViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view: View = inflater.inflate(R.layout.rv_depenses_item, parent, false)
+        return CategoryAdapter.CategoryViewHolder(itemView = view)
+    }
+
+    override fun onBindViewHolder(holder: AccountAdapter.AccountViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        return accountList.size
+    }
+    }
+
+*/
+
+
+
