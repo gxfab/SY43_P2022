4aa5a6ddf9ff9ecd0e8db4402ffb60b757211c09
FateWArd <corentin.dentzer01@gmail.com>
2022-05-25 16:09:14 +0200

    Ajout trois activités

---
+++ ZeroDay/app/src/main/java/com/example/zeroday/views/NameValidationActivity.kt
@@ -0,0 +1,12 @@
+package com.example.zeroday.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.zeroday.R
+
+class NameValidationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_name_validation)
+    }
+}
