b2f7ef809e78bdf15d9f77e0e53f98312997a2ca
Peyron <peyron.th@gmail.com>
2022-05-27 10:16:45 +0200

    Construction de l'interface d'accueil nommée overview

--- app/app/src/main/java/fr/sy/lebudgetduzero/MainActivity.kt
+++ app/app/src/main/java/fr/sy/lebudgetduzero/MainActivity.kt
@@ -2,10 +2,16 @@ package fr.sy.lebudgetduzero
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun changeView(view: View){
+    }
+
+
 }
