e98143a4355f2e0a80db4c194ee6829793d39858
lsacienne <alexandre.viala@outlook.com>
2022-05-28 18:30:41 +0200

    Revert ":sparkles: Add of chart to display a specific budget"
    This reverts commit 4d00508b9ea22f0362980d2558a445f27517c30d.

--- app/src/main/java/com/example/lafo_cheuse/fragment/view/ChartFragment.kt
+++ app/src/main/java/com/example/lafo_cheuse/fragment/view/ChartFragment.kt
@@ -1,20 +1,11 @@
 package com.example.lafo_cheuse.fragment.view
 
-import android.graphics.Color
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import com.example.lafo_cheuse.R
-import com.example.lafo_cheuse.material.RoundedBarChart
-import com.github.mikephil.charting.charts.BarChart
-import com.github.mikephil.charting.components.Description
-import com.github.mikephil.charting.components.XAxis
-import com.github.mikephil.charting.components.YAxis
-import com.github.mikephil.charting.data.*
-import com.github.mikephil.charting.utils.ColorTemplate
-
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -27,7 +18,6 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class ChartFragment : Fragment() {
-    var chartView : BarChart? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -35,49 +25,5 @@ class ChartFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_chart, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        chartView = view.findViewById<BarChart>(R.id.specBudgetChart)
-        configureChartAppearance()
-
-        val entries = ArrayList<BarEntry>()
-
-        val x : Float = 0.0F
-        val y: Float = (Math.random() * 70 + 30).toFloat()
-        entries.add(BarEntry(x, y))
-
-
-        val dataSet = BarDataSet(entries, "")
-
-        dataSet.colors = ColorTemplate.PASTEL_COLORS.toList()
-        dataSet.valueTextColor = Color.WHITE
-        dataSet.valueTextSize = 12f
-
-        val data = BarData(dataSet)
-
-        chartView!!.data = data
-        val description = Description()
-        description.textColor = Color.RED
-        description.text = "Bootleg graph"
-
-        chartView!!.legend.isEnabled = false
-        chartView!!.description = description
-        chartView!!.animateX(2000)
-    }
-
-    private fun configureChartAppearance() {
-        chartView!!.renderer = RoundedBarChart(chartView,chartView!!.animator,chartView!!.viewPortHandler, 50)
-        chartView!!.description.isEnabled = false
-        chartView!!.setDrawValueAboveBar(false)
-        val xAxis: XAxis = chartView!!.xAxis
-
-        val axisLeft: YAxis = chartView!!.axisLeft
-        axisLeft.granularity = 10f
-        axisLeft.axisMinimum = 0f
-        val axisRight: YAxis = chartView!!.axisRight
-        axisRight.granularity = 10f
-        axisRight.axisMinimum = 0f
     }
 }
