de9bf2d7b7d64934e298d8b518ef443843e6f2d1
visclo44 <clovis.burgaud@gmail.com>
2022-05-28 20:08:51 +0200

    working bottom menu

--- nomoola/app/src/main/java/com/example/nomoola/activity/MainActivity.java
+++ nomoola/app/src/main/java/com/example/nomoola/activity/MainActivity.java
@@ -2,11 +2,17 @@ package com.example.nomoola.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
 import com.example.nomoola.R;
 import com.example.nomoola.viewModel.CategoryViewModel;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +23,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        this.mCatViewModel = new ViewModelProvider(this).get(CategoryViewModel.class);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView);
+        NavController navController = navHostFragment.getNavController();
+
+
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.homePage, R.id.historyPage, R.id.profilePage).build();
+
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
     }
 }
