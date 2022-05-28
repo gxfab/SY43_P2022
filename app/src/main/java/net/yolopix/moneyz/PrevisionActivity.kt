2ec6d4a6d1419101ff1d9c2f091cf95d3487d248
elios-cama <elios.cama@gmail.com>
2022-05-28 17:56:35 +0200

    creation de activité prevision

--- app/src/main/java/net/yolopix/moneyz/PrevisionActivity.kt
+++ app/src/main/java/net/yolopix/moneyz/PrevisionActivity.kt
@@ -2,10 +2,18 @@ package net.yolopix.moneyz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.google.android.material.slider.Slider
 
 class PrevisionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_prevision)
+        var slider = findViewById<com.google.android.material.slider.Slider>(R.id.salary_slider)
+        var text_salary = findViewById<TextView>(R.id.salary_text);
+
+        slider.addOnChangeListener { slider, value, fromUser ->
+            text_salary.text = String.format("%.2f", value.toDouble())
+        }
     }
 }
