8317afc245fd69d81cd8cc54a6e14e8da36576fa
Peyron <peyron.th@gmail.com>
2022-05-30 11:35:36 +0200

    Ajout des premiers éléments sur la overview

--- app/app/src/main/java/fr/sy/lebudgetduzero/MainActivity.kt
+++ app/app/src/main/java/fr/sy/lebudgetduzero/MainActivity.kt
@@ -3,15 +3,31 @@ package fr.sy.lebudgetduzero
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    //Data nécessaire en test
+    private val tx : Int = 60
+    private val money : Float= 1566.85F
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.view_overview)
+
+        setHomeView(null)
     }
 
-    fun setHomeView(view: View){
+    fun setHomeView(view: View?){
         setContentView(R.layout.view_overview)
+
+        //Mise à jour de l'élément du taux restants dans le mois
+        val textViewTaux : TextView = findViewById(R.id.B_DepenseTaux) as TextView
+        textViewTaux.text= "Vous avez dépensé " + this.tx + "% de votre budget mensuel"
+
+        //Mise à jour de l'argent restant
+        val textViewArgent : TextView = findViewById(R.id.B_MoneyAvailable) as TextView
+        textViewArgent.text= "${this.money}€ disponible"
+
     }
 
     fun setBalanceView(view: View){
