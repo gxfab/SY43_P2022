f0088f265f65ae74f123e19c47f3228449e5715a
William Mann <william.mann@utbm.fr>
2022-05-30 17:46:35 +0200

    Interface work

--- App/app/src/main/java/com/example/econo_misons/ChangerBudget.java
+++ App/app/src/main/java/com/example/econo_misons/ChangerBudget.java
@@ -1,13 +1,14 @@
 package com.example.econo_misons;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ChangerBudget extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_changer_budget);
     }
 }
