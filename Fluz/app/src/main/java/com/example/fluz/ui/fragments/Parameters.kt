72db7e5bb6bd906740bbbb36a0c243e06e04ff17
Maureen-gdd <maureen.grandidier@gmail.com>
2022-05-31 10:30:54 +0200

    Add fragment to navgraph

--- Fluz/app/src/main/java/com/example/fluz/ui/fragments/Parameters.kt
+++ Fluz/app/src/main/java/com/example/fluz/ui/fragments/Parameters.kt
@@ -1,6 +1,22 @@
 package com.example.fluz.ui.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.fluz.databinding.FragmentParametersBinding
 
 class Parameters : Fragment() {
+    private lateinit var binding: FragmentParametersBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentParametersBinding.inflate(layoutInflater)
+
+        return binding.root
+    }
 }
--- Fluz/app/src/main/java/com/example/fluz/ui/fragments/SpendingHistory.kt
+++ Fluz/app/src/main/java/com/example/fluz/ui/fragments/SpendingHistory.kt
@@ -1,6 +1,22 @@
 package com.example.fluz.ui.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.fluz.databinding.FragmentSpendingHistoryBinding
 
 class SpendingHistory : Fragment() {
+    private lateinit var binding: FragmentSpendingHistoryBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentSpendingHistoryBinding.inflate(layoutInflater)
+
+        return binding.root
+    }
 }
