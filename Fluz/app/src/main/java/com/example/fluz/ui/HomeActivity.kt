23e8933228e2761bf2510db104bfe1b84f34541b
Maureen-gdd <maureen.grandidier@gmail.com>
2022-05-31 10:39:12 +0200

    NAVIGATION IS WORKING *cry*

--- Fluz/app/src/main/java/com/example/fluz/ui/HomeActivity.kt
+++ Fluz/app/src/main/java/com/example/fluz/ui/HomeActivity.kt
@@ -9,9 +9,11 @@ import android.view.MenuItem
 import android.view.View
 import androidx.navigation.findNavController
 import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupWithNavController
 import com.example.fluz.R
 import com.example.fluz.databinding.ActivityHomeBinding
 import com.example.fluz.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
@@ -21,5 +23,11 @@ class HomeActivity : AppCompatActivity() {
 
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //Initialize the bottom navigation view
+        //create bottom navigation view object
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navController = findNavController(R.id.nav_host_fragment_content_home_main)
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
