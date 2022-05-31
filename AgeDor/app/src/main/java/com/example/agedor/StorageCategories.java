a8f719929e647f343b8f668a9a5b47cc957b4a0c
LeoViguier <80176209+LeoViguier@users.noreply.github.com>
2022-05-31 16:35:37 +0200

    tout pour BDD

---
+++ AgeDor/app/src/main/java/com/example/agedor/StorageCategories.java
@@ -0,0 +1,13 @@
+package com.example.agedor;
+
+public class StorageCategories {
+    public String nom;
+    public Double montant;
+    public int id;
+
+    // Constructeur pour notre classe
+    public StorageCategories(String nom, Double montant) {
+        this.nom = nom;
+        this.montant = montant;
+    }
+}
