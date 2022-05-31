75a95daecf930d91c1acd8ebd61e3ecbf78ed08a
rosen902 <corentin.ehlinger@gmail.com>
2022-05-31 11:05:29 +0200

    Ajout activité permettant de lister une catégorie

---
+++ ZeroDay/app/src/main/java/com/example/zeroday/views/CategoryActivity.java
@@ -0,0 +1,15 @@
+package com.example.zeroday.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category);
+
+    }
+}
