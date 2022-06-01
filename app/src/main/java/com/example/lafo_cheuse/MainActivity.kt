5e261300682aa5bde9512746924944652b1371d4
lsacienne <alexandre.viala@outlook.com>
2022-06-02 01:13:52 +0200

    :heavy_plus_sign: Add of dependency in the MainActivity

--- app/src/main/java/com/example/lafo_cheuse/MainActivity.kt
+++ app/src/main/java/com/example/lafo_cheuse/MainActivity.kt
@@ -1,29 +1,20 @@
 package com.example.lafo_cheuse
 
-import android.graphics.Color
 import android.os.Bundle
-import android.view.MenuItem
-import android.view.View
-import androidx.annotation.Nullable
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
 import androidx.fragment.app.Fragment
 import androidx.room.Room
+import androidx.viewpager2.widget.ViewPager2
 import com.example.lafo_cheuse.database.LafoCheuseDatabase
 import com.example.lafo_cheuse.fragment.view.ChartFragment
 import com.example.lafo_cheuse.fragment.view.HomeFragment
 import com.example.lafo_cheuse.fragment.view.SetIncomesExpensesFragment
 import com.example.lafo_cheuse.fragment.view.SettingsFragment
+import com.example.lafo_cheuse.material.ViewPagerAdapter
 import com.example.lafo_cheuse.models.Category
-import com.github.mikephil.charting.charts.PieChart
-import com.github.mikephil.charting.components.Description
-import com.github.mikephil.charting.data.PieData
-import com.github.mikephil.charting.data.PieDataSet
-import com.github.mikephil.charting.data.PieEntry
-import com.github.mikephil.charting.utils.ColorTemplate
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.navigation.NavigationBarView
 import kotlinx.coroutines.*
 
 
