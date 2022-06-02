a7a3574899f76934333af8fd760604f4b879ea34
Peyron <peyron.th@gmail.com>
2022-06-02 17:01:38 +0200

    Adding of comments to generate documentation

--- app/app/src/main/java/fr/sy/lebudgetduzero/MainActivity.kt
+++ app/app/src/main/java/fr/sy/lebudgetduzero/MainActivity.kt
@@ -5,6 +5,12 @@ import android.os.Bundle
 import android.view.View
 import android.widget.TextView
 
+/**
+ *
+ * Manage the main activity content.
+ *
+ * @constructor Set the content of the activity to Home View (overview layout)
+ */
 class MainActivity : AppCompatActivity() {
     //Data nécessaire en test
     private val tx : Int = 60
@@ -17,6 +23,11 @@ class MainActivity : AppCompatActivity() {
         setHomeView(null)
     }
 
+    /**
+     * Set the content view as the home view using the overview layout.
+     *
+     * @param [view] Actual view of the app.
+     */
     fun setHomeView(view: View?){
         setContentView(R.layout.view_overview)
 
@@ -26,18 +37,33 @@ class MainActivity : AppCompatActivity() {
 
         //Mise à jour de l'argent restant
         val textViewArgent : TextView = findViewById(R.id.B_MoneyAvailable) as TextView
-        textViewArgent.text= "${this.money}€ disponible"
+        textViewArgent.text= this.money.toString() + "€ disponible"
 
     }
 
+    /**
+     * Set the content view as the balance view using the overview layout.
+     *
+     * @param [view] Acual view of the app.
+     */
     fun setBalanceView(view: View){
         setContentView(R.layout.view_balance)
     }
 
+    /**
+     * Set the content view as the input view using the overview layout.
+     *
+     * @param [view] Actual view of the app.
+     */
     fun setEntreeView(view: View){
         setContentView(R.layout.view_entrees)
     }
 
+    /**
+     * Set the content view as the output view using the overview layout.
+     *
+     * @param [view] Actual view of the app.
+     */
     fun setDepenseView(view: View){
         setContentView(R.layout.view_depense)
     }
