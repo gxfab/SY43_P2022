e3e64fb6dbc16ca045164b40489da8e61bbd80e2
lsacienne <alexandre.viala@outlook.com>
2022-06-02 23:42:07 +0200

    :sparkles: Creation of BudgetSetter activity.

---
+++ app/src/main/java/com/example/lafo_cheuse/BudgetSetterActivity.kt
@@ -0,0 +1,20 @@
+package com.example.lafo_cheuse
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.lafo_cheuse.databinding.ActivityBudgetSetterBinding
+
+class BudgetSetterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_budget_setter)
+
+
+    }
+}
