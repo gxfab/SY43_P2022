168b977c8375fc5df92880a30469854ca316d502
Christian DO <christian.do2101@gmail.com>
2022-06-03 15:14:11 +0200

    Avancement barre saisie et boutons retour
    Ajout d'une barre de saisie dans l'activité  "saisie" et ajout d'un bouton de retour dans la même activité.
    Ajout d'un bouton supplémentaire à l'accueil et les titres des trois boutons sont bons.

--- CookUTProject/app/src/main/java/com/example/cookutproject/Saisie.java
+++ CookUTProject/app/src/main/java/com/example/cookutproject/Saisie.java
@@ -2,7 +2,9 @@ package com.example.cookutproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Saisie extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class Saisie extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saisie);
+    }
+    public void changeActivity(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
