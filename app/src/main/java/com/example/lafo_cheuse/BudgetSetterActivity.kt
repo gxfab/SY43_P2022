776ce40a6f6533fad53d77b7af134c012c3d5c83
lsacienne <alexandre.viala@outlook.com>
2022-06-04 16:49:26 +0200

    :sparkles: Add of possibility to generate fragments by clicking on a button in BudgetSetterActivity

--- app/src/main/java/com/example/lafo_cheuse/BudgetSetterActivity.kt
+++ app/src/main/java/com/example/lafo_cheuse/BudgetSetterActivity.kt
@@ -1,19 +1,40 @@
 package com.example.lafo_cheuse
 
+import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.view.View
+import android.widget.ImageButton
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import com.example.lafo_cheuse.databinding.ActivityBudgetSetterBinding
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+import com.example.lafo_cheuse.fragment.view.BudgetSetterFragment
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
 
 class BudgetSetterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_budget_setter)
+
+        val  addRegularIncomeButton : ImageButton = findViewById(R.id.addRegularIncome)
+
+        addRegularIncomeButton.setOnClickListener {
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add<BudgetSetterFragment>(R.id.regularIncomeView)
+            }
+        }
+
+        val addRegularExpenseButton : ImageButton = findViewById(R.id.addRegularExpense)
+
+        addRegularExpenseButton.setOnClickListener {
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add<BudgetSetterFragment>(R.id.regularExpenseView)
+            }
+        }
 
 
     }
