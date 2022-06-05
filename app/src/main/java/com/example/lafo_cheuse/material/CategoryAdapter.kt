b4543343b6b7172810a1e66f8a8916a0e30b8ac5
lsacienne <alexandre.viala@outlook.com>
2022-06-05 18:12:26 +0200

    :sparkles: Add of adapter used by the RecyclerView used by the Category's Recycler view

---
+++ app/src/main/java/com/example/lafo_cheuse/material/CategoryAdapter.kt
@@ -0,0 +1,53 @@
+package com.example.lafo_cheuse.material
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.lifecycle.LiveData
+import androidx.recyclerview.widget.RecyclerView
+import com.example.lafo_cheuse.R
+import com.example.lafo_cheuse.models.Category
+
+// Create the basic adapter extending from RecyclerView.Adapter
+// Note that we specify the custom ViewHolder which gives us access to our views
+class CategoryAdapter() : RecyclerView.Adapter<CategoryAdapter.ViewHolder>() {
+    private var mCategories: List<Category> = ArrayList<Category>()
+
+    // Provide a direct reference to each of the views within a data item
+    // Used to cache the views within the item layout for fast access
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        // Your holder should contain and initialize a member variable
+        // for any view that will be set as you render a row
+        val categoryName: TextView = itemView.findViewById<TextView>(R.id.categoryName)
+        val categoryEmojiButton: Button = itemView.findViewById<Button>(R.id.emojiButton)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val context = parent.context
+        val inflater = LayoutInflater.from(context)
+        // Inflate the custom layout
+        val categoryItemView = inflater.inflate(R.layout.category_item, parent, false)
+        // Return a new holder instance
+        return ViewHolder(categoryItemView)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        // Get the data model based on position
+        val category: Category = mCategories[position]
+        // Set item views based on your views and data model
+        holder.categoryName.text = category.categoryName
+        holder.categoryEmojiButton.text = category.categoryEmoji
+    }
+
+    override fun getItemCount(): Int {
+        return mCategories.size;
+
+    }
+
+    fun setCategories(mCategories : List<Category>) {
+        this.mCategories = mCategories
+        notifyDataSetChanged();
+    }
+}
