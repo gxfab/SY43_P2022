921d06883ec4d63e739fb07c03be91d240740a79
visclo44 <clovis.burgaud@gmail.com>
2022-06-06 16:07:19 +0200

    Profile update

--- nomoola/app/src/main/java/com/example/nomoola/activity/ProfileActivity.java
+++ nomoola/app/src/main/java/com/example/nomoola/activity/ProfileActivity.java
@@ -2,65 +2,38 @@ package com.example.nomoola.activity;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.nomoola.R;
+import com.example.nomoola.fragment.CategoryFragment;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileActivity#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class ProfileActivity extends Fragment {
+public class ProfileActivity extends AppCompatActivity {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ProfileActivity() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment profilePage.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProfileActivity newInstance(String param1, String param2) {
-        ProfileActivity fragment = new ProfileActivity();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private String Name;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
+        Name = getNameFromDb();
+        setProfileName(Name);
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        setContentView(R.layout.activity_profile);
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_profile, container, false);
+    private String getNameFromDb(){
+        Name = "John Moola";
+        return Name;
+    }
+
+    private void setProfileName(String Name){
+        final TextView textViewToChange = (TextView) findViewById(R.id.profileName);
+        textViewToChange.setText(Name);
     }
 }
