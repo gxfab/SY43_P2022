33fbe05ad3964fe652f29a5c84d76ef849b0a7d2
lsacienne <alexandre.viala@outlook.com>
2022-06-08 12:35:38 +0200

    :card_file_box: Modification of constructor to fit with MoneyChange

--- app/src/main/java/com/example/lafo_cheuse/models/Expense.kt
+++ app/src/main/java/com/example/lafo_cheuse/models/Expense.kt
@@ -3,8 +3,8 @@ package com.example.lafo_cheuse.models
 import androidx.room.Entity
 
 @Entity
-class Expense(frequency: Frequency, category: Category, amount: Double) :
-    MoneyChange(frequency, category) {
+class Expense(frequency: Frequency, name : String, category: Category, amount: Double) :
+    MoneyChange(frequency, name,category) {
     var amount: Double = 0.0
         set(value){
             if (value <= 0.0) {
