0bc7454962e3d4d5d4a6fa4022a92751659b2d0b
lsacienne <alexandre.viala@outlook.com>
2022-06-08 12:33:52 +0200

    :card_file_box: Modification of database in Income.kt

--- app/src/main/java/com/example/lafo_cheuse/models/Income.kt
+++ app/src/main/java/com/example/lafo_cheuse/models/Income.kt
@@ -5,12 +5,12 @@ import androidx.room.Entity
 import java.lang.Exception
 
 @Entity
-class Income(frequency: Frequency, category: Category, amount: Double) :
-    MoneyChange(frequency, category) {
+class Income(frequency: Frequency,name : String, category: Category, amount: Double) :
+    MoneyChange(frequency,name, category) {
     @ColumnInfo(name = "amount")
     var amount: Double = 0.0
         set(value){
-            if (value > 0.0) {
+            if (value >= 0.0) {
                 field = value
             } else {
                 throw Exception("The amount is negative!")
