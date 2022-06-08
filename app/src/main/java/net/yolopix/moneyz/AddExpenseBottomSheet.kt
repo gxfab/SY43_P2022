2142d4a996a468a58be27cf16436f89b1e8df5ea
Yolopix <13918281+y0lopix@users.noreply.github.com>
2022-06-08 23:24:23 +0200

    Check for errors in expense add details form

--- app/src/main/java/net/yolopix/moneyz/AddExpenseBottomSheet.kt
+++ app/src/main/java/net/yolopix/moneyz/AddExpenseBottomSheet.kt
@@ -5,19 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
-import android.widget.ArrayAdapter
-import android.widget.CheckBox
-import android.widget.EditText
-import android.widget.Spinner
+import android.widget.*
+import androidx.core.widget.addTextChangedListener
 import androidx.lifecycle.lifecycleScope
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.textfield.TextInputLayout
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import net.yolopix.moneyz.model.AppDatabase
 import net.yolopix.moneyz.model.entities.Category
 import net.yolopix.moneyz.model.entities.Expense
 import net.yolopix.moneyz.model.entities.Month
+import java.time.LocalDate
 
 /**
  * A bottom sheet dialog fragment containing widgets
@@ -36,23 +36,35 @@ class AddExpenseBottomSheet(private val db: AppDatabase, private val month: Mont
         return inflater.inflate(R.layout.bottom_sheet_add_expense, container, false)
     }
 
+    // Widgets
     private lateinit var editTextExpenseName: EditText
     private lateinit var editTextExpenseAmount: EditText
     private lateinit var editTextExpenseDay: EditText
     private lateinit var recurringCheckBox: CheckBox
     private lateinit var categorySpinner: Spinner
+    private lateinit var nameTextInputLayout: TextInputLayout
+    private lateinit var amountTextInputLayout: TextInputLayout
+    private lateinit var dateTextInputLayout: TextInputLayout
+    private lateinit var buttonAdd: Button
 
-    // When the bottom sheet is created
+    /**
+     * When the bottom sheet view is created
+     */
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val buttonAdd =
-            view.findViewById<com.google.android.material.button.MaterialButton>(R.id.button_add)
         editTextExpenseName = view.findViewById(R.id.edit_text_expense_name)
         editTextExpenseAmount = view.findViewById(R.id.edit_text_expense_price)
-        recurringCheckBox = view.findViewById(R.id.checkBox_recurring)
         editTextExpenseDay = view.findViewById(R.id.editTextDate)
+        recurringCheckBox = view.findViewById(R.id.checkBox_recurring)
         categorySpinner = view.findViewById(R.id.spinner_category)
+        nameTextInputLayout = view.findViewById(R.id.text_input_layout_name)
+        amountTextInputLayout = view.findViewById(R.id.text_input_layout_amount)
+        dateTextInputLayout = view.findViewById(R.id.text_input_layout_date)
+        buttonAdd = view.findViewById(R.id.button_add)
+
+        // Pre-fill details
+        editTextExpenseDay.setText(LocalDate.now().dayOfMonth.toString())
 
         // Add click listener to the add button
         buttonAdd.setOnClickListener {
@@ -66,13 +78,39 @@ class AddExpenseBottomSheet(private val db: AppDatabase, private val month: Mont
             true
         }
 
+        // Error checking
+        editTextExpenseName.addTextChangedListener {
+            nameTextInputLayout.error =
+                if (it.isNullOrEmpty()) getString(R.string.error_empty_name) else null
+            checkFormErrors()
+        }
+
+
+        editTextExpenseAmount.addTextChangedListener {
+            amountTextInputLayout.error = when (it.toString().toFloatOrNull()) {
+                null -> getString(R.string.error_empty_text)
+                0f -> getString(R.string.error_amount_zero)
+                else -> null
+            }
+            checkFormErrors()
+        }
+
+        val monthAsLocalDate: LocalDate = LocalDate.of(month.yearNumber, month.monthNumber, 1)
+        editTextExpenseDay.addTextChangedListener {
+            dateTextInputLayout.error = when (it.toString().toIntOrNull()) {
+                null -> getString(R.string.error_empty_text)
+                in 1..monthAsLocalDate.lengthOfMonth() -> null
+                else -> getString(R.string.error_invalid_day_of_month)
+            }
+            checkFormErrors()
+        }
+
         // Date picker
         val datePicker = MaterialDatePicker.Builder.datePicker()
             .setTitleText(R.string.expense_date)
             .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
             .build()
 
-        //val adapter = ArrayAdapter.createFromResource(requireContext(), android.R.string ,android.R.layout.item_category)
         lifecycleScope.launch {
             categorySpinner.adapter = ArrayAdapter(
                 requireContext(),
@@ -91,7 +129,7 @@ class AddExpenseBottomSheet(private val db: AppDatabase, private val month: Mont
         val expenseDay = editTextExpenseDay.text.toString().toInt()
         val recurring = recurringCheckBox.isChecked
         val category = categorySpinner.selectedItem as Category
-        //if(expensePrice.toFloat() > db.categoryDao().getPredictedAmount())
+
         val newExpense = Expense(0, expenseDay, category.uid, expenseName, recurring, expensePrice)
 
         runBlocking {
@@ -103,6 +141,15 @@ class AddExpenseBottomSheet(private val db: AppDatabase, private val month: Mont
         }
 
         dismiss()
+    }
+
+    private fun checkFormErrors() {
+        buttonAdd.isEnabled = nameTextInputLayout.error == null
+                && amountTextInputLayout.error == null
+                && dateTextInputLayout.error == null
+                && editTextExpenseName.text.isNotBlank()
+                && editTextExpenseAmount.text.isNotBlank()
+                && editTextExpenseDay.text.isNotBlank()
     }
 
 
