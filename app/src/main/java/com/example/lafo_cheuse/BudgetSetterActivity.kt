3edbfde1845d8f3c67c7766624a6939082d1caee
lsacienne <alexandre.viala@outlook.com>
2022-06-09 01:26:05 +0200

    :recycle: Refactor code to work with two recycler view + initialization of recycler view in dedicated functions

--- app/src/main/java/com/example/lafo_cheuse/BudgetSetterActivity.kt
+++ app/src/main/java/com/example/lafo_cheuse/BudgetSetterActivity.kt
@@ -2,61 +2,62 @@ package com.example.lafo_cheuse
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.ImageButton
 import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.add
-import androidx.fragment.app.commit
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.lafo_cheuse.fragment.view.BudgetSetterFragment
+import com.example.lafo_cheuse.material.ExpenseSetterAdapter
 import com.example.lafo_cheuse.material.IncomeSetterAdapter
-import com.example.lafo_cheuse.models.Income
 import com.example.lafo_cheuse.viewmodels.CategoryViewModel
+import com.example.lafo_cheuse.viewmodels.ExpenseViewModel
 import com.example.lafo_cheuse.viewmodels.IncomeViewModel
 
 
 class BudgetSetterActivity : AppCompatActivity() {
-    private var adapter : IncomeSetterAdapter? = null
-    private var resultLauncher : ActivityResultLauncher<Intent>? = null
+    private var incomeAdapter : IncomeSetterAdapter? = null
+    private var expenseAdapter : ExpenseSetterAdapter? = null
     private val incomeViewModel : IncomeViewModel by viewModels()
+    private val expenseViewModel : ExpenseViewModel by viewModels()
     private val categoryViewModel : CategoryViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_budget_setter)
 
-        val recyclerView : RecyclerView = findViewById<RecyclerView>(R.id.income_list_recycler_view)
-        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false)
-        recyclerView.setHasFixedSize(true)
-
-        adapter = IncomeSetterAdapter(this,incomeViewModel)
-        recyclerView.adapter = adapter
+        initializeRecyclerViewIncome()
+        initializeRecyclerViewExpense()
 
         categoryViewModel.getDefaultCategory()?.observe(this) { list ->
-            adapter!!.setDefCategory(list[0])
+            incomeAdapter!!.setDefCategory(list[0])
+            expenseAdapter!!.setDefCategory(list[0])
         }
-
-        incomeViewModel.getIncomes()?.observe(this) { list ->
-            adapter!!.setIncomes(list)
-        }
-
-
-        val addRegularExpenseButton : ImageButton = findViewById(R.id.addRegularExpense)
-
-        addRegularExpenseButton.setOnClickListener {
-            supportFragmentManager.commit {
-                setReorderingAllowed(true)
-                add<BudgetSetterFragment>(R.id.regularExpenseView)
-            }
-        }
-
 
     }
 
-    fun startIntent(income : Income) {
+    private fun initializeRecyclerViewIncome() {
+        val recyclerViewIncome : RecyclerView = findViewById(R.id.income_list_recycler_view)
+        recyclerViewIncome.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false)
+        recyclerViewIncome.setHasFixedSize(true)
 
+        incomeAdapter = IncomeSetterAdapter(this,incomeViewModel)
+        recyclerViewIncome.adapter = incomeAdapter
+
+        incomeViewModel.getIncomes()?.observe(this) { list ->
+            incomeAdapter!!.setIncomes(list)
+        }
+    }
+
+    private fun initializeRecyclerViewExpense() {
+        val recyclerViewExpense : RecyclerView = findViewById(R.id.expense_list_recycler_view)
+        recyclerViewExpense.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false)
+        recyclerViewExpense.setHasFixedSize(true)
+
+        expenseAdapter = ExpenseSetterAdapter(this,expenseViewModel)
+        recyclerViewExpense.adapter = expenseAdapter
+
+        expenseViewModel.getExpenses()?.observe(this) { list ->
+            expenseAdapter!!.setExpenses(list)
+        }
     }
 }
