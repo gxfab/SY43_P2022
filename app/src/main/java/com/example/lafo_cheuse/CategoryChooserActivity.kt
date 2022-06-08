2c8db006156be840fe6d4f6931609429ef108a01
lsacienne <alexandre.viala@outlook.com>
2022-06-08 12:39:17 +0200

    :sparkles: Add of gestion of bundle to link with the income

--- app/src/main/java/com/example/lafo_cheuse/CategoryChooserActivity.kt
+++ app/src/main/java/com/example/lafo_cheuse/CategoryChooserActivity.kt
@@ -5,21 +5,36 @@ import android.os.Bundle
 import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.lafo_cheuse.material.CategoryAdapter
 import com.example.lafo_cheuse.models.Category
+import com.example.lafo_cheuse.models.Income
 import com.example.lafo_cheuse.viewmodels.CategoryViewModel
+import com.example.lafo_cheuse.viewmodels.IncomeViewModel
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
 class CategoryChooserActivity : AppCompatActivity() {
+    val incomeViewModel : IncomeViewModel by viewModels()
+    var income : Income? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_category_chooser)
+        val b = intent.extras
+        val moneyChangeId : Long
+        val type : String
+
+        if (b != null){
+            moneyChangeId = b.getLong("moneyChangeId")
+            type = b.getString("type")!!
+            if(type == "income") {
+                incomeViewModel.getIncome(moneyChangeId).observe(this) { list ->
+                    income = list[0]
+                }
+            }
+        }
 
         val adapter : CategoryAdapter?
 
@@ -42,5 +57,11 @@ class CategoryChooserActivity : AppCompatActivity() {
         }
 
 
+    }
+
+    fun chooseCategory(categoryChosen : Category) {
+        income?.category = categoryChosen
+        incomeViewModel.updateIncome(income!!)
+        this.finish()
     }
 }
