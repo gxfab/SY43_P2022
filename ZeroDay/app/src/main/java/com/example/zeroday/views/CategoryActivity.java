4f5173ab6769f2192034da23c8ad48a2793fbcf3
rosen902 <corentin.ehlinger@gmail.com>
2022-06-08 16:01:03 +0200

    Commit avant fusion

--- ZeroDay/app/src/main/java/com/example/zeroday/views/CategoryActivity.java
+++ ZeroDay/app/src/main/java/com/example/zeroday/views/CategoryActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.zeroday.R;
+
 public class CategoryActivity extends AppCompatActivity {
 
     @Override
