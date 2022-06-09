33afaa96a518a0b5b3b38e9f639cb9f793d413de
lsacienne <alexandre.viala@outlook.com>
2022-06-09 20:10:51 +0200

    :sparkles: Management of database data in homeFragment There are 3 modes : - Incomes : all incomes from user - budgetedExpenses : expenses that were planned - oneTimeExpenses : current state in expenses

--- app/src/main/java/com/example/lafo_cheuse/fragment/view/HomeFragment.kt
+++ app/src/main/java/com/example/lafo_cheuse/fragment/view/HomeFragment.kt
@@ -6,26 +6,34 @@ import android.transition.TransitionInflater
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
+import androidx.fragment.app.viewModels
 import com.example.lafo_cheuse.MainActivity
 import com.example.lafo_cheuse.R
+import com.example.lafo_cheuse.models.ExpenseSumContainer
+import com.example.lafo_cheuse.models.Income
+import com.example.lafo_cheuse.viewmodels.CategoryViewModel
+import com.example.lafo_cheuse.viewmodels.ExpenseViewModel
+import com.example.lafo_cheuse.viewmodels.IncomeViewModel
 import com.github.mikephil.charting.charts.PieChart
 import com.github.mikephil.charting.components.Description
-import com.github.mikephil.charting.data.PieData
-import com.github.mikephil.charting.data.PieDataSet
-import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.data.*
 import com.github.mikephil.charting.utils.ColorTemplate
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.coroutines.DelicateCoroutinesApi
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-
 class HomeFragment() : Fragment() {
+    private val incomeViewModel : IncomeViewModel by viewModels()
+    private val expenseViewModel : ExpenseViewModel by viewModels()
+
+    private var incomes : List<Income>? = null
+    private var budgetedExpenses : List<ExpenseSumContainer>? = null
+    private var oneTimeExpenses : List<ExpenseSumContainer>? = null
+
+    private val listGraphNames : List<String> = listOf("Revenus","Dépenses prévues","Dépenses effectives")
+    private var indexSelectedGraph : Int = 0
+
     var chartView : PieChart? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,46 +49,104 @@ class HomeFragment() : Fragment() {
     ): View {
         val view : View = inflater.inflate(R.layout.fragment_home, container, false)
         val chartButton : FloatingActionButton = view.findViewById(R.id.floatingActionButton)
-        chartButton.setOnClickListener { v ->
-            displayChart(v)
+        val switchButton : FloatingActionButton = view.findViewById(R.id.switchGraphHome)
+        val typeDisplayed : TextView = view.findViewById(R.id.budetTypeDisplayer)
+        typeDisplayed.text = listGraphNames[indexSelectedGraph]
+        chartButton.setOnClickListener {
+            displayChart()
         }
 
+        switchButton.setOnClickListener {
+            incrementIndex()
+            typeDisplayed.text = listGraphNames[indexSelectedGraph]
+            watchBudget()
+        }
         return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-
-
         super.onViewCreated(view, savedInstanceState)
         chartView = view.findViewById<PieChart>(R.id.piechart)
-        chartView?.setTouchEnabled(false);
+        watchBudget()
+    }
 
-        val entries = ArrayList<PieEntry>()
+    @OptIn(DelicateCoroutinesApi::class)
+    fun displayChart() {
+        (this.activity as MainActivity).navigationView!!.selectedItemId = R.id.chartFragment
+    }
 
-        for (i in 0..3) {
-            entries.add(PieEntry((Math.random() * 70 + 30).toFloat(), "\uD83D\uDED2 Quarter " + (i + 1)))
+    fun incrementIndex() {
+        indexSelectedGraph = (indexSelectedGraph + 1) % listGraphNames.size
+    }
+
+    private fun watchBudget() {
+        when(listGraphNames[indexSelectedGraph]) {
+            "Revenus"-> {
+                incomeViewModel.getIncomes()?.observe(viewLifecycleOwner) { mIncomes ->
+                    incomes = mIncomes
+                    createGraph(convertIncomesInList(incomes!!))
+                }
+            }
+            "Dépenses prévues"-> {
+                expenseViewModel.getMonthlyExpensesSumByCategory().observe(viewLifecycleOwner) { mExpenses ->
+                    budgetedExpenses = mExpenses
+                    createGraph(convertExpensesInList(budgetedExpenses!!))
+                }
+             }
+            "Dépenses effectives" -> {
+                expenseViewModel.getOneTimeExpensesSumByCategory().observe(viewLifecycleOwner) { mExpenses ->
+                    oneTimeExpenses = mExpenses
+                    createGraph(convertExpensesInList(oneTimeExpenses!!))
+                }
+            }
         }
-        val dataSet = PieDataSet(entries, "")
 
-        dataSet.colors = ColorTemplate.PASTEL_COLORS.toList()
-        dataSet.valueTextColor = Color.WHITE
-        dataSet.valueTextSize = 12f
+    }
 
+    private fun createGraph(entries : ArrayList<PieEntry>) {
+        configureChartAppearance()
+
+        val dataSet = setGraphData(entries)
         val data = PieData(dataSet)
 
         chartView!!.data = data
         val description = Description()
         description.textColor = Color.RED
-        description.text = "Bootleg graph"
+        description.text = ""
 
         chartView!!.legend.isEnabled = false
         chartView!!.description = description
         chartView!!.animateX(2000)
     }
 
-    @OptIn(DelicateCoroutinesApi::class)
-    fun displayChart(view : View) {
-        (this.activity as MainActivity).navigationView!!.selectedItemId = R.id.chartFragment
+    private fun convertIncomesInList(mIncomes : List<Income>) : ArrayList<PieEntry> {
+        val entries = ArrayList<PieEntry>()
+        for(income in mIncomes) {
+            entries.add(PieEntry(income.amount.toFloat(), income.name+" "+ (income.category?.categoryEmoji)))
+        }
+        return entries
+    }
+
+    private fun convertExpensesInList(mExpenses : List<ExpenseSumContainer>) : ArrayList<PieEntry> {
+        val entries = ArrayList<PieEntry>()
+        for(expense in mExpenses) {
+            entries.add(PieEntry(-expense.totalAmount.toFloat(), expense.category_name+" "+ (expense.category_emoji)))
+        }
+        return entries
+    }
+
+    private fun setGraphData(entries : ArrayList<PieEntry>) : PieDataSet {
+        val dataSet = PieDataSet(entries, "")
+
+        dataSet.colors = ColorTemplate.PASTEL_COLORS.toList()
+        dataSet.valueTextColor = Color.WHITE
+        dataSet.valueTextSize = 12f
+        return dataSet
+    }
+
+    private fun configureChartAppearance() {
+        chartView?.setTouchEnabled(false)
+        chartView!!.description.isEnabled = false
     }
 
 }
