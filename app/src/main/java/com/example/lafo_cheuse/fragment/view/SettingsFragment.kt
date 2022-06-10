5121abbff60ce8ec2992019ee47590fdc990a9aa
lsacienne <alexandre.viala@outlook.com>
2022-06-11 00:56:51 +0200

    :lipstick: Add of aesthetic settings fragment

--- app/src/main/java/com/example/lafo_cheuse/fragment/view/SettingsFragment.kt
+++ app/src/main/java/com/example/lafo_cheuse/fragment/view/SettingsFragment.kt
@@ -1,29 +1,29 @@
 package com.example.lafo_cheuse.fragment.view
 
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.example.lafo_cheuse.R
+import org.w3c.dom.Text
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SettingsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SettingsFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+    ): View {
+        val view : View = inflater.inflate(R.layout.fragment_settings, container, false)
+        val optionIcon : TextView = view.findViewById(R.id.optionIcon)
+        val optionGithub : TextView = view.findViewById(R.id.optionGithub)
+
+        optionIcon.movementMethod = LinkMovementMethod.getInstance()
+        optionGithub.movementMethod = LinkMovementMethod.getInstance()
+
+        return view
     }
 }
