5daf9e7e8101650385f6ee23e47a7b3cf514842c
lsacienne <alexandre.viala@outlook.com>
2022-06-11 17:05:40 +0200

    :lipstick: Support of spinner to choose the day

--- app/src/main/java/com/example/lafo_cheuse/fragment/view/SettingsFragment.kt
+++ app/src/main/java/com/example/lafo_cheuse/fragment/view/SettingsFragment.kt
@@ -6,6 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListAdapter
+import android.widget.Spinner
 import android.widget.TextView
 import com.example.lafo_cheuse.R
 import org.w3c.dom.Text
@@ -20,10 +23,23 @@ class SettingsFragment : Fragment() {
         val view : View = inflater.inflate(R.layout.fragment_settings, container, false)
         val optionIcon : TextView = view.findViewById(R.id.optionIcon)
         val optionGithub : TextView = view.findViewById(R.id.optionGithub)
+        val optionSpinner : Spinner = view.findViewById(R.id.option_spinner)
+        setupSpinner(optionSpinner)
 
         optionIcon.movementMethod = LinkMovementMethod.getInstance()
         optionGithub.movementMethod = LinkMovementMethod.getInstance()
 
         return view
+    }
+
+    fun setupSpinner(spinner : Spinner) {
+        val day_list : Array<Int> = (1..31).toList().toTypedArray()
+        val adapter : ArrayAdapter<Int> = ArrayAdapter(requireContext(),
+            android.R.layout.simple_spinner_item,
+            day_list)
+        spinner.adapter = adapter
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
     }
 }
