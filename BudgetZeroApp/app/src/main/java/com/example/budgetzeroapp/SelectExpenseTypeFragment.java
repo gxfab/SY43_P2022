f9e845949810931d607871ece0eadeec94411869
Molrn <youri.aubry@utbm.fr>
2022-06-11 17:24:06 +0200

    Activity to Fragments

---
+++ BudgetZeroApp/app/src/main/java/com/example/budgetzeroapp/SelectExpenseTypeFragment.java
@@ -0,0 +1,41 @@
+package com.example.budgetzeroapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RadioGroup;
+
+import android.app.Activity;
+
+import androidx.fragment.app.Fragment;
+
+
+public class SelectExpenseTypeFragment extends Fragment {
+
+    private RadioGroup radioGroup;
+    Button submit;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_select_expense_type, parent, false);
+
+        submit = view.findViewById(R.id.submit);
+        radioGroup = view.findViewById(R.id.group_radio_type);
+
+        submit.setOnClickListener(v -> {
+            int selectedId = radioGroup.getCheckedRadioButtonId();
+            int type;
+            if(selectedId == R.id.type_exp) type = DBHelper.TYPE_EXP;
+            else if(selectedId == R.id.type_inc) type = DBHelper.TYPE_INC;
+            else if(selectedId == R.id.type_debt) type = DBHelper.TYPE_DEBT;
+            else type = DBHelper.TYPE_SAV;
+
+            //Léo: ouvrir ici page DisplayExpense en passant la variable "type"
+        });
+
+        return view;
+    }
+}
