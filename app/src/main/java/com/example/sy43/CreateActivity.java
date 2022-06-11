8cf78365278cd65c66aabf638dfc4ea32229f3a0
Clamore <rotuman.lenoob@gmail.com>
2022-06-12 01:46:40 +0200

    objectives can be added

--- app/src/main/java/com/example/sy43/CreateActivity.java
+++ app/src/main/java/com/example/sy43/CreateActivity.java
@@ -19,6 +19,7 @@ public class CreateActivity  extends AppCompatActivity {
         setContentView(R.layout.activity_create);
         CategoryViewModel categoryViewModel = new ViewModelProvider(this).get(CategoryViewModel .class);
         categoryViewModel.init();
+
         Button cat = (Button) findViewById(R.id.btnCatConfirm);
         cat.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,6 +29,7 @@ public class CreateActivity  extends AppCompatActivity {
                 cat.setCatName(name.getText().toString());
                 cat.setMaxValue(50);
                 cat.setCurrentValue(5);
+                cat.setObjective(false);
                 categoryViewModel.createCategory(cat);
                 Intent intent = new Intent(v.getContext(), CategoryActivity.class);
                 v.getContext().startActivity(intent);
@@ -37,6 +39,13 @@ public class CreateActivity  extends AppCompatActivity {
         obj.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Categorydb cat = new Categorydb();
+                TextView name = (TextView) findViewById(R.id.objName);
+                cat.setCatName(name.getText().toString());
+                cat.setMaxValue(50);
+                cat.setCurrentValue(5);
+                cat.setObjective(true);
+                categoryViewModel.createCategory(cat);
                 Intent intent = new Intent(v.getContext(), CategoryActivity.class);
                 v.getContext().startActivity(intent);
             }
