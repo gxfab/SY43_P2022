31c39643923265c00976f2e4271952b32fd60bb9
SavageBlink <drisskihal@gmail.com>
2022-06-12 00:15:03 +0200

    Successfully implemented a random PieChart in a positive spending fragment

--- Sucelloz/app/src/main/java/com/sucelloztm/sucelloz/ui/spendings/PositiveSpendingsFragment.java
+++ Sucelloz/app/src/main/java/com/sucelloztm/sucelloz/ui/spendings/PositiveSpendingsFragment.java
@@ -1,11 +1,18 @@
 package com.sucelloztm.sucelloz.ui.spendings;
 
 
-import android.app.Activity;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.RelativeSizeSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,6 +23,8 @@ import androidx.navigation.fragment.NavHostFragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
 import com.sucelloztm.sucelloz.databinding.PositiveSpendingsFragmentBinding;
 import com.sucelloztm.sucelloz.models.InfrequentExpensesAndIncome;
 import com.sucelloztm.sucelloz.ui.dialogs.AddSpendingDialogFragment;
@@ -36,11 +45,46 @@ public class PositiveSpendingsFragment extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
-    public PieChart createPieChart(Activity activity)
+    public PieChart createPieChart()
     {
+
+        Context context = getContext();
+        PieChart pieChart = new PieChart(context);
+        pieChart.getDescription().setEnabled(false);
+
+        Typeface tf = Typeface.create((Typeface) null,Typeface.NORMAL);
+
+        pieChart.setCenterTextTypeface(tf);
+        pieChart.setCenterText(generateCenterText());
+        pieChart.setCenterTextSize(10f);
+        pieChart.setCenterTextTypeface(tf);
+
+        // radius of the center hole in percent of maximum radius
+        pieChart.setHoleRadius(45f);
+        pieChart.setTransparentCircleRadius(50f);
+
+        Legend l = pieChart.getLegend();
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.RIGHT);
+        l.setOrientation(Legend.LegendOrientation.VERTICAL);
+        l.setDrawInside(false);
+
+        pieChart.setData(spendingsViewModel.generatePieData(tf));
+
+        FrameLayout parent = binding.frameLayoutPositiveSpendings;
+        parent.addView(pieChart);
+
+        return pieChart;
 
     }
 
+
+    private SpannableString generateCenterText() {
+        SpannableString s = new SpannableString("Revenues\nQuarters 2015");
+        s.setSpan(new RelativeSizeSpan(2f), 0, 8, 0);
+        s.setSpan(new ForegroundColorSpan(Color.GRAY), 8, s.length(), 0);
+        return s;
+    }
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -49,6 +93,10 @@ public class PositiveSpendingsFragment extends Fragment {
         spendingsViewModel = new ViewModelProvider(this).get(SpendingsViewModel.class);
         currentPositiveSpendingsList = new ArrayList<>();
         PositiveSpendingsAdapter adapter = new PositiveSpendingsAdapter(currentPositiveSpendingsList);
+
+        PieChart pieChart = createPieChart();
+        pieChart.invalidate();
+
         final Observer<List<InfrequentExpensesAndIncome>> positiveSpendingsDataSet = new Observer<List<InfrequentExpensesAndIncome>>() {
             @Override
             public void onChanged(List<InfrequentExpensesAndIncome> infrequentExpensesAndIncomes) {
@@ -64,11 +112,15 @@ public class PositiveSpendingsFragment extends Fragment {
                 });
                 adapter.notifyDataSetChanged();
             }
+
+
         };
         spendingsViewModel.getAllPositiveSpendings().observe(getViewLifecycleOwner(),positiveSpendingsDataSet);
         recyclerView = binding.spendingsRecyclerView;
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         recyclerView.setAdapter(adapter);
+
+
 
         return root;
     }
--- Sucelloz/app/src/main/java/com/sucelloztm/sucelloz/ui/spendings/SpendingsViewModel.java
+++ Sucelloz/app/src/main/java/com/sucelloztm/sucelloz/ui/spendings/SpendingsViewModel.java
@@ -1,16 +1,23 @@
 package com.sucelloztm.sucelloz.ui.spendings;
 
 import android.app.Application;
+import android.graphics.Color;
+import android.graphics.Typeface;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.sucelloztm.sucelloz.models.InfrequentExpensesAndIncome;
 import com.sucelloztm.sucelloz.models.SubCategories;
 import com.sucelloztm.sucelloz.repositories.InfrequentExpensesAndIncomeRepository;
 import com.sucelloztm.sucelloz.repositories.SubCategoriesRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SpendingsViewModel extends AndroidViewModel {
@@ -47,5 +54,27 @@ public class SpendingsViewModel extends AndroidViewModel {
 
     public String getSubCategoryNameWithId(long idOfSubCategory){
         return subCategoriesRepository.getSubCategoryNameWithId(idOfSubCategory);
+    }
+
+    public PieData generatePieData(Typeface tf) {
+
+        int count = 4;
+
+        ArrayList<PieEntry> entries1 = new ArrayList<>();
+
+        for(int i = 0; i < count; i++) {
+            entries1.add(new PieEntry((float) ((Math.random() * 60) + 40), "Quarter " + (i+1)));
+        }
+
+        PieDataSet ds1 = new PieDataSet(entries1, "Quarterly Revenues 2015");
+        ds1.setColors(ColorTemplate.VORDIPLOM_COLORS);
+        ds1.setSliceSpace(2f);
+        ds1.setValueTextColor(Color.WHITE);
+        ds1.setValueTextSize(12f);
+
+        PieData d = new PieData(ds1);
+        d.setValueTypeface(tf);
+
+        return d;
     }
 }
