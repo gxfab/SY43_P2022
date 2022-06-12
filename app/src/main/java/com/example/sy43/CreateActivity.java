6b7ce2bf26d0e4c5e233978103bb6cc753be1f17
Clamore <rotuman.lenoob@gmail.com>
2022-06-12 15:51:57 +0200

    add calendar

--- app/src/main/java/com/example/sy43/CreateActivity.java
+++ app/src/main/java/com/example/sy43/CreateActivity.java
@@ -2,12 +2,16 @@ package com.example.sy43;
 
 import static java.lang.Float.parseFloat;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
@@ -15,11 +19,27 @@ import androidx.lifecycle.ViewModelProvider;
 import com.example.sy43.db.entity.Categorydb;
 import com.example.sy43.viewmodels.CategoryViewModel;
 
+import java.util.Calendar;
+
 public class CreateActivity  extends AppCompatActivity {
+
+    private DatePicker datePicker;
+    private Calendar calendar;
+    private TextView dateView;
+    private int year, month, day;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
+
+        dateView = (TextView) findViewById(R.id.textView3);
+        calendar = Calendar.getInstance();
+        year = calendar.get(Calendar.YEAR);
+
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+        showDate(year, month+1, day);
+
         CategoryViewModel categoryViewModel = new ViewModelProvider(this).get(CategoryViewModel .class);
         categoryViewModel.init();
 
@@ -57,6 +77,42 @@ public class CreateActivity  extends AppCompatActivity {
                 v.getContext().startActivity(intent);
             }
         });
+    }
+
+    @SuppressWarnings("deprecation")
+    public void setDate(View view) {
+        showDialog(999);
+        Toast.makeText(getApplicationContext(), "ca",
+                        Toast.LENGTH_SHORT)
+                .show();
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        // TODO Auto-generated method stub
+        if (id == 999) {
+            return new DatePickerDialog(this,
+                    myDateListener, year, month, day);
+        }
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener myDateListener = new
+            DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker arg0,
+                                      int arg1, int arg2, int arg3) {
+                    // TODO Auto-generated method stub
+                    // arg1 = year
+                    // arg2 = month
+                    // arg3 = day
+                    showDate(arg1, arg2+1, arg3);
+                }
+            };
+
+    private void showDate(int year, int month, int day) {
+        dateView.setText(new StringBuilder().append(day).append("/")
+                .append(month).append("/").append(year));
     }
 
 
