14611906d67365c6febc98efe4e51b8a1ce85f21
SkyMound <crafteurkiller@gmail.com>
2022-06-13 14:37:53 +0200

    too many issues

--- gestimali/app/src/main/java/com/example/gestimali/MainActivity.kt
+++ gestimali/app/src/main/java/com/example/gestimali/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.Window
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
