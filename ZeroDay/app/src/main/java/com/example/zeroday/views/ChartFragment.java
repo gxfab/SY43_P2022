6cd880e41a84520b91ccb4dadf8b53246a016ab7
rosen902 <corentin.ehlinger@gmail.com>
2022-06-15 00:04:34 +0200

    Ajout du RadarChart

--- ZeroDay/app/src/main/java/com/example/zeroday/views/ChartFragment.java
+++ ZeroDay/app/src/main/java/com/example/zeroday/views/ChartFragment.java
@@ -12,9 +12,15 @@ import androidx.fragment.app.Fragment;
 import com.example.zeroday.R;
 import com.github.mikephil.charting.animation.Easing;
 import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.charts.RadarChart;
+import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.data.RadarData;
+import com.github.mikephil.charting.data.RadarDataSet;
+import com.github.mikephil.charting.data.RadarEntry;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
 import com.github.mikephil.charting.formatter.PercentFormatter;
 
 import java.util.ArrayList;
@@ -22,6 +28,7 @@ import java.util.ArrayList;
 public class ChartFragment extends Fragment {
 
     private PieChart pieChart;
+    private RadarChart radarChart;
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -33,18 +40,71 @@ public class ChartFragment extends Fragment {
         configPieChart();
         loadPieChart();
 
+        radarChart = view.findViewById(R.id.radarchart);
+        configRadarChart();
+        loadRadarChart();
+
+
         return view;
     }
 
     private void configPieChart(){
-            pieChart.setDrawHoleEnabled(true);
-            pieChart.setUsePercentValues(true);
-            pieChart.setEntryLabelTextSize(12);
-            pieChart.setEntryLabelColor(Color.WHITE);
-            pieChart.getDescription().setEnabled(false);
-            pieChart.getLegend().setEnabled(false);
-            pieChart.setRotationEnabled(false);
+        pieChart.setDrawHoleEnabled(true);
+        pieChart.setUsePercentValues(true);
+        pieChart.setEntryLabelTextSize(12);
+        pieChart.setEntryLabelColor(Color.WHITE);
+        pieChart.getDescription().setEnabled(false);
+        pieChart.getLegend().setEnabled(false);
+        pieChart.setRotationEnabled(false);
     }
+
+    private void configRadarChart(){
+
+        radarChart.setRotationEnabled(false);
+    }
+
+    private void loadRadarChart(){
+
+        ArrayList<RadarEntry> currentDataVals = new ArrayList<>();
+        currentDataVals.add(new RadarEntry(3));
+        currentDataVals.add(new RadarEntry(7));
+        currentDataVals.add(new RadarEntry(2));
+        currentDataVals.add(new RadarEntry(5));
+        currentDataVals.add(new RadarEntry(3));
+        currentDataVals.add(new RadarEntry(1));
+
+        ArrayList<RadarEntry> lastDataVals = new ArrayList<>();
+        lastDataVals.add(new RadarEntry(2));
+        lastDataVals.add(new RadarEntry(6));
+        lastDataVals.add(new RadarEntry(3));
+        lastDataVals.add(new RadarEntry(4));
+        lastDataVals.add(new RadarEntry(6));
+        lastDataVals.add(new RadarEntry(4));
+
+        RadarDataSet currentDataSet = new RadarDataSet(currentDataVals, "Current expenses");
+        RadarDataSet lastDataSet = new RadarDataSet(lastDataVals, "Last cycle expenses");
+
+        currentDataSet.setColor(ContextCompat.getColor(getContext(), R.color.zero_purple));
+        lastDataSet.setColors(ContextCompat.getColor(getContext(), R.color.zero_pink0));
+
+        RadarData data = new RadarData();
+        data.addDataSet(currentDataSet);
+        data.addDataSet(lastDataSet);
+
+        String[] labels = {"Hobbies","Health","Transports","Food","House","Custom"};
+        XAxis xAxis = radarChart.getXAxis();
+        xAxis.setValueFormatter(new IndexAxisValueFormatter(labels));
+
+        radarChart.setData(data);
+        radarChart.invalidate();
+        radarChart.animateX(800, Easing.EaseInOutQuad);
+        radarChart.animateY(800, Easing.EaseInOutQuad);
+
+
+    }
+
+
+
 
     private void loadPieChart(){
 
