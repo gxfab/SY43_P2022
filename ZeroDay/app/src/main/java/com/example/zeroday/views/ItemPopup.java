56061547174ee9442d6baaabdb8a1b27ad0e4a41
rosen902 <corentin.ehlinger@gmail.com>
2022-06-14 20:42:47 +0200

    Ajout du spinner pour la popup item

--- ZeroDay/app/src/main/java/com/example/zeroday/views/ItemPopup.java
+++ ZeroDay/app/src/main/java/com/example/zeroday/views/ItemPopup.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.Dialog;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -18,17 +19,25 @@ public class ItemPopup extends Dialog {
     private Spinner spinner;
     private Button cancel;
     private Button add;
+    String[] cycles;
 
     public ItemPopup(Activity activity)
     {
         super(activity, androidx.appcompat.R.style.Theme_AppCompat_Dialog);
         this.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         setContentView(R.layout.item_popup_template);
+
+
         this.title = "Add Item";
         this.spinner = findViewById(R.id.item_popup_spinner);
         this.label = findViewById(R.id.item_popup_label);
         this.cancel = findViewById(R.id.item_popup_cancel);
         this.add = findViewById(R.id.item_popup_add);
+        this.cycles = new String[]{"cycle1", "cycle2", "cycle3", "cycle4"};
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(getContext(), R.layout.black_spinner_item, cycles);
+        arrayAdapter.setDropDownViewResource(androidx.appcompat.R.layout.support_simple_spinner_dropdown_item);
+        spinner.setAdapter(arrayAdapter);
 
     }
 
