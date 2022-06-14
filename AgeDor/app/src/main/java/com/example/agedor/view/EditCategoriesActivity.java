fa7e50907732239579dd1a75fca43993ab7b97e0
Léo Viguier <leo.viguier@live.fr>
2022-06-14 23:47:44 +0200

    UI Sur les modifications des données

---
+++ AgeDor/app/src/main/java/com/example/agedor/view/EditCategoriesActivity.java
@@ -0,0 +1,58 @@
+package com.example.agedor.view;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.text.InputType;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewStub;
+import android.widget.EditText;
+
+import com.example.agedor.R;
+
+public class EditCategoriesActivity extends AppCompatActivity {
+    public String m_Text = "";
+    public Double m_Montant = 0.0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edit_categories);
+    }
+
+
+    public void changeCategorie(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = this.getLayoutInflater();
+
+        builder.setView(inflater.inflate(R.layout.dialog_edit_categorie, null))
+                // Add action buttons
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        m_Text = ((EditText) ((AlertDialog) dialog).findViewById(R.id.nom)).getText().toString();
+                        m_Montant = Double.parseDouble(((EditText) ((AlertDialog) dialog).findViewById(R.id.montant)).getText().toString());
+                        Log.i("EditCategoriesActivity", "m_Text: " + m_Text);
+                        Log.i("EditCategoriesActivity", "m_Montant: " + m_Montant);
+                    }
+                })
+                .setNegativeButton("Annuler", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                        Log.i("EditCategoriesActivity", "Annuler");
+                    }
+                });
+        builder.create();
+        builder.show();
+    }
+
+
+    public void addCategorie(View view) {
+        Log.i("EditCategoriesActivity", "addCategorie");
+    }
+
+}
---
+++ AgeDor/app/src/main/java/com/example/agedor/view/EditRevenusActivity.java
@@ -0,0 +1,14 @@
+package com.example.agedor.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.agedor.R;
+
+public class EditRevenusActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.editer_budget);
+    }
+}
