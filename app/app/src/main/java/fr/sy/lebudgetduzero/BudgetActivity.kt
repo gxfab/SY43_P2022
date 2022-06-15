7300d84da2446944df19f0603b11880bd22a09dc
Peyron <peyron.th@gmail.com>
2022-06-15 10:51:24 +0200

    Finishing of activity to manage budget prevision.

--- app/app/src/main/java/fr/sy/lebudgetduzero/BudgetActivity.kt
+++ app/app/src/main/java/fr/sy/lebudgetduzero/BudgetActivity.kt
@@ -1,16 +1,20 @@
 package fr.sy.lebudgetduzero
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import fr.sy.lebudgetduzero.database.AppDatabase
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.launch
+import java.lang.Math.abs
 import java.text.SimpleDateFormat
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
@@ -64,10 +68,30 @@ class BudgetActivity : AppCompatActivity() {
                     (findViewById<EditText>(idInput)).text = Editable.Factory.getInstance()
                         .newEditable(requestInfo.value_for_month.toString())
 
+                    //Setting listener on change
+                    (findViewById<EditText>(idInput)).addTextChangedListener(object : TextWatcher{
+                        override fun beforeTextChanged(
+                            p0: CharSequence?,
+                            p1: Int,
+                            p2: Int,
+                            p3: Int
+                        ) {    }
+
+                        override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                        }
+
+
+                        override fun afterTextChanged(p0: Editable?) {
+                            updateRemaining(null)
+                        }
+                    })
+
                     //Setting LastMonth spend value
                     val idValue =resources.getIdentifier(("lastMonthType$idType"), "id", packageName)
                     (findViewById<TextView>(idValue)).text =
                         requestSpent.toString() + "€ dépensés dans cette catégorie le mois passé."
+
+                    updateRemaining(null)
                 }
             }
             //Setting income
@@ -77,10 +101,52 @@ class BudgetActivity : AppCompatActivity() {
             )
             findViewById<EditText>(R.id.editIncome).text = Editable.Factory.getInstance().newEditable(incomeLastMonth.toString())
         }
+
+    }
+
+    fun updateRemaining(view:View?){
+
+        //Count of estimated spend global value
+        var spend=0
+        var income=0F
+        var color="FFFFFF"
+        for (idType in 1..11) {
+            val idInput = resources.getIdentifier(("valueType$idType"), "id", packageName)
+            try {
+                spend+=findViewById<EditText>(idInput).text.toString().toInt()
+            }catch (e: Exception) {}
+
+        }
+
+        //Input estimated
+        income=findViewById<EditText>(R.id.editIncome).text.toString().toFloat()
+
+        val budget=income-spend
+
+        val textView=findViewById<TextView>(R.id.remainingMoney)
+        val buttonSave=findViewById<Button>(R.id.buttonSave)
+
+
+        if(budget==0F){
+            textView.setTextColor(Color.parseColor("#00FF00"))
+            textView.text="Vous n'avez plus d'argent à placer dans vos catégories bravo"
+            buttonSave.isEnabled=true
+        }else if(budget<0F){
+            textView.setTextColor(Color.parseColor("#FF0000"))
+            textView.text="Vous avez alloué "+ Math.abs(budget).toString() + "€ en trop."
+            buttonSave.isEnabled=false
+        }else{
+            textView.setTextColor(Color.parseColor("#EE2222"))
+            textView.text="Il vous reste $budget€ à allouer."
+            buttonSave.isEnabled=false
+        }
+
+
+
     }
 
 
-    fun setBudget(view: View) {
+    fun setBudget(view: View?) {
         val applicationScope = CoroutineScope(SupervisorJob())
         applicationScope.launch {
             val db = AppDatabase.getDatabase(applicationContext)
