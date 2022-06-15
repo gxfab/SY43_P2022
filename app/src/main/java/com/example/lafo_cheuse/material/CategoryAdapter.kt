5babe01a3d0561f0ae91e404b3511c8a2d602776
lsacienne <alexandre.viala@outlook.com>
2022-06-15 23:50:18 +0200

    :memo: Completion of documentation in CategoryAdapter.kt file

--- app/src/main/java/com/example/lafo_cheuse/material/CategoryAdapter.kt
+++ app/src/main/java/com/example/lafo_cheuse/material/CategoryAdapter.kt
@@ -12,20 +12,30 @@ import com.example.lafo_cheuse.CategoryChooserActivity
 import com.example.lafo_cheuse.R
 import com.example.lafo_cheuse.models.Category
 
-// Create the basic adapter extending from RecyclerView.Adapter
-// Note that we specify the custom ViewHolder which gives us access to our views
+/**
+ * [RecyclerView.Adapter] for the CategoryChooserActivity [RecyclerView]
+ *
+ * @property context - [Activity] context of the adapter
+ * @property mCategories - list of [Category] containing the [Category] to display
+ */
 class CategoryAdapter(var context : Activity) : RecyclerView.Adapter<CategoryAdapter.ViewHolder>() {
     private var mCategories: List<Category> = ArrayList<Category>()
 
-    // Provide a direct reference to each of the views within a data item
-    // Used to cache the views within the item layout for fast access
+    /**
+     * Inner class that will hold the widgets of one [RecyclerView] item.
+     *
+     * @param itemView - a [View] representing an item of the layout
+     */
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        // Your holder should contain and initialize a member variable
-        // for any view that will be set as you render a row
         val categoryName: TextView = itemView.findViewById<TextView>(R.id.categoryName)
         val categoryEmojiButton: Button = itemView.findViewById<Button>(R.id.emojiButton)
     }
 
+    /**
+     * Callback function called when the [ViewHolder] is created to let it initialized himself correctly
+     *
+     * @return a [ViewHolder] initialized
+     */
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val context = parent.context
         val inflater = LayoutInflater.from(context)
@@ -35,6 +45,12 @@ class CategoryAdapter(var context : Activity) : RecyclerView.Adapter<CategoryAda
         return ViewHolder(categoryItemView)
     }
 
+    /**
+     * Callback function called when the [holder] is initialized to display data on elements at [position]
+     *
+     * @param holder - [ViewHolder] holding all the elements
+     * @param position - [Int] containing the index of the [View] in [mCategories]
+     */
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         // Get the data model based on position
         val category: Category = mCategories[position]
@@ -47,11 +63,21 @@ class CategoryAdapter(var context : Activity) : RecyclerView.Adapter<CategoryAda
         }
     }
 
+    /**
+     * Callback function which send the number of elements in the [RecyclerView]
+     *
+     * @return [Int] - the size of [mCategories]
+     */
     override fun getItemCount(): Int {
         return mCategories.size
 
     }
 
+    /**
+     * Update categories in the [RecyclerView] by updating [mCategories]
+     *
+     * @param mCategories - a list of [Category]
+     */
     fun setCategories(mCategories : List<Category>) {
         this.mCategories = mCategories
         notifyDataSetChanged()
