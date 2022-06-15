1de8daa59e02c330397a3f92e77b953fb2f40a6a
lsacienne <alexandre.viala@outlook.com>
2022-06-15 23:51:47 +0200

    :memo: Add of documentation in ViewPagerAdapter.kt file

--- app/src/main/java/com/example/lafo_cheuse/material/ViewPagerAdapter.kt
+++ app/src/main/java/com/example/lafo_cheuse/material/ViewPagerAdapter.kt
@@ -5,6 +5,14 @@ import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
+/**
+ * Adapter to implement a ViewPager2 on the MainActivity menu.
+ *
+ *
+ * @param list - a list [Fragment] to indicates which fragment the viewPager should make focus on
+ * @param fm - [FragmentManager]
+ * @param lifecycle - [Lifecycle] that the adapter should follow
+ */
 class ViewPagerAdapter(
     list: ArrayList<Fragment>,
     fm: FragmentManager,
@@ -13,10 +21,21 @@ class ViewPagerAdapter(
 
     private val fragmentList : ArrayList<Fragment> = list
 
+    /**
+     * Callback function called to know the number of item to switch between
+     *
+     * @return
+     */
     override fun getItemCount(): Int {
         return fragmentList.size
     }
 
+    /**
+     * Callback function called to create a fragment in the list of fragment (create = display it)
+     *
+     * @param position - [Int] position of the fragment in [fragmentList]
+     * @return a [Fragment] at position [position] in [fragmentList]
+     */
     override fun createFragment(position: Int): Fragment {
         return fragmentList[position]
     }
