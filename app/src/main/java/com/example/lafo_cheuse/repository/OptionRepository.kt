adc2ff1b757ccda54a3d38edf781b3154868a01d
lsacienne <alexandre.viala@outlook.com>
2022-06-15 21:48:16 +0200

    :memo: Documentation of OptionRepository.kt file

--- app/src/main/java/com/example/lafo_cheuse/repository/OptionRepository.kt
+++ app/src/main/java/com/example/lafo_cheuse/repository/OptionRepository.kt
@@ -10,6 +10,17 @@ import com.example.lafo_cheuse.models.OptionField
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
+/**
+ * ViewModel of the options and optionFields
+ *
+ * @property optionDao - An [OptionDao] instance which will make the queries
+ * @property optionFieldDao - AN [OptionField] instance which will make the queries
+ * @property allOptions - A [LiveData] list of [Option] in database
+ *
+ * @constructor It will initialize all the properties with the [LiveData] asked from the [optionDao]
+ *
+ * @param application - The [Application] which it is linked with
+ */
 class OptionRepository(application: Application) {
     private var optionDao: OptionDao?
     private var optionFieldDao: OptionFieldDao?
@@ -22,22 +33,51 @@ class OptionRepository(application: Application) {
         allOptions = optionDao?.getOptions()
     }
 
+    /**
+     * Method to get all the options.
+     *
+     * @return [allOptions] which has been initialized in the constructor
+     */
     fun getOptions() : LiveData<List<Option>>? {
         return allOptions
     }
 
+    /**
+     * Method to get a particular [Option]
+     *
+     * @param optionName - [String] with the name of desired [Option]
+     * @return a [LiveData] container of [Option]
+     */
     fun getOption(optionName: String) : LiveData<Option>? {
         return optionDao?.getOption(optionName)
     }
 
+    /**
+     * Method to get all the [OptionField] linked with an [Option]
+     *
+     * @param option - the [Option] we want to get the [OptionField] of
+     * @return a [LiveData] list of [OptionField]
+     */
     fun getOptionFields(option : Option) : LiveData<List<OptionField>>? {
         return optionFieldDao?.getOptionFields(option.optionDescription)
     }
 
+    /**
+     * Method to update an [OptionField] in database
+     *
+     * @param field - [OptionField] to update
+     * @return [Unit] nothing
+     */
     suspend fun updateOptionField(field : OptionField) = withContext(Dispatchers.IO) {
         optionFieldDao?.updateOptionField(field)
     }
 
+    /**
+     * Method to get all the [OptionField] linked with an [Option] synchronously
+     *
+     * @param option - the [Option] we want to get the [OptionField] of
+     * @return a list of [OptionField]
+     */
     suspend fun getOptionFieldsSync(option : Option) : List<OptionField>? {
         return optionFieldDao?.getOptionFieldsSync(option.optionDescription)
     }
