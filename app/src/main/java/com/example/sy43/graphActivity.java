529827faadde3c6e00902cc36f7e98061b134ee1
Clamore <rotuman.lenoob@gmail.com>
2022-06-16 00:53:47 +0200

    add graph

--- app/src/main/java/com/example/sy43/graphActivity.java
+++ app/src/main/java/com/example/sy43/graphActivity.java
@@ -1,12 +1,18 @@
 package com.example.sy43;
 
+import android.os.Bundle;
 import android.util.Log;
+import android.widget.ListView;
 
+import com.example.sy43.adapters.CategoryAdapter;
 import com.example.sy43.db.entity.Categorydb;
 import com.example.sy43.db.entity.Transaction;
 import com.example.sy43.db.mainDB.DB;
 import com.example.sy43.db.mainDB.DBexec;
 import com.example.sy43.repositories.CategoryRepository;
+import com.example.sy43.viewmodels.CategoryViewModel;
+import com.example.sy43.viewmodels.SubCategoryViewModel;
+import com.example.sy43.viewmodels.TransactionViewModel;
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
@@ -16,40 +22,49 @@ import com.google.common.util.concurrent.Futures;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class graphActivity extends AppCompatActivity{
-    private DB db;
-    private DBexec databaseExecutor;
-    PieChart chart = (PieChart) findViewById(R.id.graph);
-    ArrayList<PieEntry> entries = new ArrayList<>();
 
-    public void onCreate() {
-        Log.d("Test", "aled");
-        Futures.addCallback(
-                db.CategoryDAO().findCategories(),
-                new FutureCallback<List<Categorydb>>() {
-                    public void onSuccess(List<Categorydb> result) {
-                        ;
-                        for (int i = 0; i < result.size(); i++) {
-                            entries.add(new PieEntry(result.get(i).CurrentValue(), result.get(i).getCatName()));
-                        }
-                        PieDataSet set = new PieDataSet(entries, "Election Results");
-                        PieData data = new PieData(set);
-                        chart.setData(data);
-                        chart.invalidate();
-                    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.graph);
+        PieChart chart = (PieChart) findViewById(R.id.graph);
+        ArrayList<PieEntry> entries = new ArrayList<>();
+        CategoryViewModel categoryViewModel = new ViewModelProvider(this).get(CategoryViewModel.class);
+        categoryViewModel.init();
 
-                    public void onFailure(@NonNull Throwable thrown) {
-                        Log.d("Test", String.valueOf(thrown));
-                    }
-                },
-                //MainActivity.getAppContext().getMainExecutor()
-                databaseExecutor
-        );
+        TransactionViewModel transactionViewModel = new ViewModelProvider(this).get(TransactionViewModel.class);
+        transactionViewModel.init();
+
+        SubCategoryViewModel subCategoryViewModel = new ViewModelProvider(this).get(SubCategoryViewModel.class);
+        subCategoryViewModel.init();
+
+        LifecycleOwner owner = this;
+        categoryViewModel.getCategories().observe(this, new Observer<List<Categorydb>>() {
+            @Override
+            public void onChanged(List<Categorydb> receivedCategories) {
+                for (int i = 0; i < receivedCategories.size(); i++) {
+                    entries.add(new PieEntry(receivedCategories.get(i).CurrentValue(), receivedCategories.get(i).getCatName()));
+                }
+                PieDataSet set = new PieDataSet(entries, "Election Results");
+                PieData data = new PieData(set);
+                chart.setData(data);
+                chart.invalidate();
+                ;
+
+            }
+        });
+
+
+
     }
 
 }
