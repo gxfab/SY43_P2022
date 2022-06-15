4fa169e99e594520ec0ab889ab3815fb018d4b5e
lsacienne <alexandre.viala@outlook.com>
2022-06-15 15:57:12 +0200

    :memo: Documentation of ExpenseViewModel.kt file

--- app/src/main/java/com/example/lafo_cheuse/viewmodels/ExpenseViewModel.kt
+++ app/src/main/java/com/example/lafo_cheuse/viewmodels/ExpenseViewModel.kt
@@ -4,13 +4,21 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.viewModelScope
-import com.example.lafo_cheuse.models.Category
-import com.example.lafo_cheuse.models.Frequency
-import com.example.lafo_cheuse.models.Expense
-import com.example.lafo_cheuse.models.ExpenseSumContainer
+import com.example.lafo_cheuse.models.*
 import com.example.lafo_cheuse.repository.ExpenseRepository
+import com.example.lafo_cheuse.repository.IncomeRepository
 import kotlinx.coroutines.launch
 
+/**
+ * ViewModel of the expenses.
+ *
+ * @property repository - An [ExpenseRepository] instance which will receive the queries
+ * @property allExpenses - A [LiveData] list of [Expense] with all the expenses including monthly & one-time incomes
+ *
+ * @constructor It will initialize all the properties with the [LiveData] asked from the [repository]
+ *
+ * @param application - The [Application] which it is linked with
+ */
 class ExpenseViewModel(application : Application) : AndroidViewModel(application) {
     private var repository : ExpenseRepository
     var allExpenses : LiveData<List<Expense>>?
@@ -20,61 +28,92 @@ class ExpenseViewModel(application : Application) : AndroidViewModel(application
         allExpenses = repository.getExpenses()
     }
 
-    fun getExpenses() : LiveData<List<Expense>>? {
-        return allExpenses
+    /**
+     * Method to get a particular expense
+     *
+     * @param expenseId - [Long] value containing the ID of the expense
+     * @return a [LiveData] list of [Expense] containing the targeted [Expense]
+     */
+    fun getExpense(expenseId : Long): LiveData<List<Expense>> {
+        return repository.getExpense(expenseId)
     }
 
-    fun getExpense(moneyChangeId : Long): LiveData<List<Expense>> {
-        return repository.getExpense(moneyChangeId)
-    }
-
+    /**
+     * Method to get all the regular expenses
+     *
+     * @return a [LiveData] list of [Expense]
+     */
     fun getMonthlyExpense() : LiveData<List<Expense>> {
         return repository.getExpensesByFrequency(Frequency.OUNCE_A_MONTH)
     }
 
+    /**
+     * Method to get all the one-time expenses
+     *
+     * @return a [LiveData] list of [Expense]
+     */
     fun getOneTimeExpense() : LiveData<List<Expense>> {
         return repository.getExpensesByFrequency(Frequency.OUNCE_A_DAY)
     }
 
+    /**
+     * Insert an [Expense] in database
+     *
+     * @param expense - The [Expense] to insert in database
+     * @return [Unit] nothing
+     */
     fun insertExpense(expense : Expense) = viewModelScope.launch {
         repository.insertExpense(expense)
     }
 
+    /**
+     * Method to update an expense in the database
+     * It is executed in a coroutine in [viewModelScope]
+     *
+     * @param expense - [Expense] that will be updated
+     * @return [Unit] Nothing
+     */
     fun updateExpense(expense : Expense) = viewModelScope.launch {
         repository.updateExpense(expense)
     }
 
+    /**
+     * Method to delete an expense in the database
+     * It is executed in a coroutine in [viewModelScope]
+     *
+     * @param expense - [Expense] that will be deleted
+     * @return [Unit] Nothing
+     */
     fun deleteExpense(expense: Expense) = viewModelScope.launch {
         repository.deleteExpense(expense.moneyChangeId)
     }
 
-    fun deleteExpenseByCategory(category: Category) = viewModelScope.launch {
-        repository.deleteExpenseByCategory(category.categoryId)
-    }
-
+    /**
+     * Get a list of sum of regular expenses in a particular category
+     *
+     * @return a [LiveData] list of [ExpenseSumContainer]
+     */
     fun getMonthlyExpensesSumByCategory() : LiveData<List<ExpenseSumContainer>> {
         return repository.getExpensesSumByCategory(Frequency.OUNCE_A_MONTH)
     }
 
-    fun getOneTimeExpensesSumByCategory() : LiveData<List<ExpenseSumContainer>> {
-        return repository.getExpensesSumByCategory(Frequency.OUNCE_A_DAY)
-    }
-
+    /**
+     * Get a sum of regular expenses for a particular category
+     *
+     * @param category - the [Category] targeted
+     * @return a [LiveData] list of [ExpenseSumContainer]
+     */
     fun getMonthlyExpensesSumForCategory(category: Category) : LiveData<List<ExpenseSumContainer>> {
         return repository.getExpensesSumForCategory(Frequency.OUNCE_A_MONTH, category.categoryId)
     }
 
-    fun getOneTimeExpensesSumForCategory(category: Category) : LiveData<List<ExpenseSumContainer>> {
-        return repository.getExpensesSumForCategory(Frequency.OUNCE_A_DAY, category.categoryId)
-    }
-
-    fun getOneTimeExpenseAndMonth(
-        year : Int,
-        month : Int
-    ) : LiveData<List<Expense>> {
-        return repository.getExpenseByFrequencyAndMonth(Frequency.OUNCE_A_DAY,year,month)
-    }
-
+    /**
+     * Get a list of sum of one-time expenses for a particular month
+     *
+     * @param year - [Int] containing the year of the month we want to show
+     * @param month - [Int] containing the month (from 0 to 11) we want to show
+     * @return a [LiveData] list of [ExpenseSumContainer]
+     */
     fun getOneTimeExpensesSumByCategoryAndMonth(
         year: Int,
         month: Int
@@ -82,6 +121,14 @@ class ExpenseViewModel(application : Application) : AndroidViewModel(application
         return repository.getExpensesSumByCategoryAndMonth(Frequency.OUNCE_A_DAY,year,month)
     }
 
+    /**
+     * Get a list of sum of one-time expenses for a particular month and a particular category
+     *
+     * @param category - [Category] of the targeted category
+     * @param year - [Int] containing the year of the month we want to show
+     * @param month - [Int] containing the month (from 0 to 11) we want to show
+     * @return a [LiveData] list of [ExpenseSumContainer]
+     */
     fun getOneTimeExpensesSumForCategoryAndMonth(
         category: Category,
         year: Int,
@@ -90,52 +137,54 @@ class ExpenseViewModel(application : Application) : AndroidViewModel(application
         return repository.getExpensesSumForCategoryAndMonth(Frequency.OUNCE_A_DAY,category,year,month)
     }
 
-    fun getMonthlyExpenseAndMonth(
-        year : Int,
-        month : Int
-    ) : LiveData<List<Expense>> {
-        return repository.getExpenseByFrequencyAndMonth(Frequency.OUNCE_A_MONTH,year,month)
-    }
-
-    fun getMonthlyExpensesSumByCategoryAndMonth(
-        year: Int,
-        month: Int
-    ) : LiveData<List<ExpenseSumContainer>> {
-        return repository.getExpensesSumByCategoryAndMonth(Frequency.OUNCE_A_MONTH,year,month)
-    }
-
-    fun getMonthlyExpensesSumForCategoryAndMonth(
-        category: Category,
-        year: Int,
-        month: Int
-    ) : LiveData<List<ExpenseSumContainer>> {
-        return repository.getExpensesSumForCategoryAndMonth(Frequency.OUNCE_A_MONTH,category,year,month)
-    }
-
-    fun getOneTimeExpensesSum() : LiveData<Double> {
-        return repository.getExpensesSumByFrequency(Frequency.OUNCE_A_DAY)
-    }
-
+    /**
+     * Get a sum of all regular expenses
+     *
+     * @return [LiveData] of [Double] containing the sum
+     */
     fun getMonthlyExpensesSum() : LiveData<Double> {
         return repository.getExpensesSumByFrequency(Frequency.OUNCE_A_MONTH)
     }
 
+    /**
+     * Get a sum of all one-time expenses for a particular month
+     *
+     * @param year - [Int] containing the year of the month we want to show
+     * @param month - [Int] containing the month (from 0 to 11) we want to show
+     * @return a [LiveData] containing a [Double]
+     */
     fun getOneTimeExpensesSumByDate(year : Int, month : Int) : LiveData<Double> {
         return repository.getExpensesSumByDate(Frequency.OUNCE_A_DAY,year,month)
     }
 
+    /**
+     * Get a sum of all regular expenses for a particular month
+     *
+     * @param year - [Int] containing the year of the month we want to show
+     * @param month - [Int] containing the month (from 0 to 11) we want to show
+     * @return a [LiveData] containing a [Double]
+     */
     fun getMonthlyExpensesSumByDate(year : Int, month : Int) : LiveData<Double> {
         return repository.getExpensesSumByDate(Frequency.OUNCE_A_MONTH,year,month)
     }
 
+    /**
+     * Get a sum of all regular expenses synchronously
+     *
+     * @return [Double] containing the sum
+     */
     suspend fun getMonthlyExpensesSumSync() : Double {
         return repository.getExpensesSumByFrequencySync(Frequency.OUNCE_A_MONTH)
     }
 
-    suspend fun getOneTimeExpensesSumByDateSync(year : Int, month : Int) : Double {
-        return repository.getExpensesSumSynchronousByDate(Frequency.OUNCE_A_DAY,year,month)
-    }
-
+    /**
+     * Get a sum a of one-time expenses for a particular month and a particular [Category] synchronously
+     *
+     * @param category - [Category] to focus on
+     * @param year - [Int] containing the year of the month we want to show
+     * @param month - [Int] containing the month (from 0 to 11) we want to show
+     * @return a list of [ExpenseSumContainer] containing only the [ExpenseSumContainer] linked with [category]
+     */
     suspend fun getOneTimeExpensesSumForCategoryAndMonthSync(
         category: Category,
         year: Int,
@@ -144,6 +193,12 @@ class ExpenseViewModel(application : Application) : AndroidViewModel(application
         return repository.getExpensesSumForCategoryAndMonthSync(Frequency.OUNCE_A_DAY,category,year,month)
     }
 
+    /**
+     * Method to delete all the expenses from the database
+     * It is executed in a coroutine in [viewModelScope]
+     *
+     * @return [Unit] Nothing
+     */
     fun wipeExpense() = viewModelScope.launch {
         repository.wipeExpense()
     }
