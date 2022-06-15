9d6e0d0732f252f7a7ec752c19d6e7983540187e
lsacienne <alexandre.viala@outlook.com>
2022-06-15 23:51:10 +0200

    :memo: Completion of documentation in FilteredEditText.kt file

--- app/src/main/java/com/example/lafo_cheuse/material/FilteredEditText.kt
+++ app/src/main/java/com/example/lafo_cheuse/material/FilteredEditText.kt
@@ -3,7 +3,11 @@ package com.example.lafo_cheuse.material
 import android.content.Context
 import android.util.AttributeSet
 
-public class FilteredEditText : androidx.appcompat.widget.AppCompatEditText {
+/**
+ * Class to implement a customized EditText
+ *
+ */
+class FilteredEditText : androidx.appcompat.widget.AppCompatEditText {
 
     constructor(context: Context, attrs: AttributeSet?, defStyle:Int):super(context,attrs,defStyle)
     {
