ed7d1c551b574a851f80324344d0ff9c11b12014
lsacienne <alexandre.viala@outlook.com>
2022-06-15 23:50:48 +0200

    :memo: Completion of documentation in EmojiFilter.kt file

--- app/src/main/java/com/example/lafo_cheuse/material/EmojiFilter.kt
+++ app/src/main/java/com/example/lafo_cheuse/material/EmojiFilter.kt
@@ -3,8 +3,21 @@ package com.example.lafo_cheuse.material
 import android.text.InputFilter
 import android.text.Spanned
 
+/**
+ * Class to create a customize EditText which filter only emojis.
+ * Every other symbols will not be printed.
+ *
+ */
 private class EmojiFilter : InputFilter {
 
+    /**
+     * Function to filter only emojis when the user type in the EditText.
+     *
+     * @param source - [CharSequence] corresponding to the text entered by the user
+     * @param start - [Int] index of the first symbol entered by user in the [source]
+     * @param end - [Int] index of the last symbol entered by the user in the [source]
+     * @return a [CharSequence] corresponding to what what was filtered
+     */
     override fun filter(
         source: CharSequence?,
         start: Int,
@@ -16,9 +29,9 @@ private class EmojiFilter : InputFilter {
         for( i in start until end) {
             val type = Character.getType(source!![i]).toByte()
             if(type != Character.SURROGATE && type != Character.OTHER_SYMBOL) {
-                return "";
+                return ""
             }
         }
-        return null;
+        return null
     }
 }
