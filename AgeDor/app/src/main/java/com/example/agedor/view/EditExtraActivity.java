1a9eacaa7265210b56eb14bc99301e354babff25
Léo Viguier <leo.viguier@live.fr>
2022-06-15 17:27:42 +0200

    Finition de la partie Editer + popup d'édit

--- AgeDor/app/src/main/java/com/example/agedor/view/EditExtraActivity.java
+++ AgeDor/app/src/main/java/com/example/agedor/view/EditExtraActivity.java
@@ -1,14 +1,74 @@
 package com.example.agedor.view;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+
 import com.example.agedor.R;
 
 public class EditExtraActivity extends AppCompatActivity {
+    public String m_Text = "";
+    public Double m_Montant = 0.0;
+    public String m_Date = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.editer_budget);
+        setContentView(R.layout.edit_extra);
     }
+
+
+    public void changeExtra(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = this.getLayoutInflater();
+
+        builder.setView(inflater.inflate(R.layout.dialog_edit_extra, null))
+                // Add action buttons
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        m_Text = ((EditText) ((AlertDialog) dialog).findViewById(R.id.nom)).getText().toString();
+                        m_Montant = Double.parseDouble(((EditText) ((AlertDialog) dialog).findViewById(R.id.montant)).getText().toString());
+                        m_Date = ((EditText) ((AlertDialog) dialog).findViewById(R.id.date)).getText().toString();
+                    }
+                })
+                .setNegativeButton("Annuler", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        builder.create();
+        builder.show();
+    }
+
+
+    public void addExtra(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = this.getLayoutInflater();
+
+        builder.setView(inflater.inflate(R.layout.dialog_edit_extra, null))
+                // Add action buttons
+                .setPositiveButton("Ajouter", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        m_Text = ((EditText) ((AlertDialog) dialog).findViewById(R.id.nom)).getText().toString();
+                        m_Montant = Double.parseDouble(((EditText) ((AlertDialog) dialog).findViewById(R.id.montant)).getText().toString());
+                        m_Date = ((EditText) ((AlertDialog) dialog).findViewById(R.id.date)).getText().toString();
+                    }
+                })
+                .setNegativeButton("Annuler", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        builder.create();
+        builder.show();
+
+    }
+
 }
--- AgeDor/app/src/main/java/com/example/agedor/view/EditProjetsActivity.java
+++ AgeDor/app/src/main/java/com/example/agedor/view/EditProjetsActivity.java
@@ -1,15 +1,71 @@
 package com.example.agedor.view;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+
 import com.example.agedor.R;
 
 public class EditProjetsActivity extends AppCompatActivity {
+    public String m_Text = "";
+    public Double m_Montant = 0.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.editer_budget);
+        setContentView(R.layout.edit_projets);
     }
-}
 
+
+    public void changeProjet(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = this.getLayoutInflater();
+
+        builder.setView(inflater.inflate(R.layout.dialog_edit_projets, null))
+                // Add action buttons
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        m_Text = ((EditText) ((AlertDialog) dialog).findViewById(R.id.nom)).getText().toString();
+                        m_Montant = Double.parseDouble(((EditText) ((AlertDialog) dialog).findViewById(R.id.montant)).getText().toString());
+                    }
+                })
+                .setNegativeButton("Annuler", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        builder.create();
+        builder.show();
+    }
+
+
+    public void addProjet(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = this.getLayoutInflater();
+
+        builder.setView(inflater.inflate(R.layout.dialog_edit_projets, null))
+                // Add action buttons
+                .setPositiveButton("Ajouter", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        m_Text = ((EditText) ((AlertDialog) dialog).findViewById(R.id.nom)).getText().toString();
+                        m_Montant = Double.parseDouble(((EditText) ((AlertDialog) dialog).findViewById(R.id.montant)).getText().toString());
+                    }
+                })
+                .setNegativeButton("Annuler", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        builder.create();
+        builder.show();
+
+    }
+
+}
