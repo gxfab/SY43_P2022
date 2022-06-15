0850aca9beec625eb1ea276e37eaf8b3c932e25f
lsacienne <alexandre.viala@outlook.com>
2022-06-15 23:50:29 +0200

    :memo: Completion of documentation in DatabaseDate.kt file

--- app/src/main/java/com/example/lafo_cheuse/material/DatabaseDate.kt
+++ app/src/main/java/com/example/lafo_cheuse/material/DatabaseDate.kt
@@ -2,6 +2,13 @@ package com.example.lafo_cheuse.material
 
 import java.util.*
 
+/**
+ * Data class to embed a date
+ *
+ * @property year - [Int] corresponding to the year of the date
+ * @property month - [Int] corresponding to the year of the date (0 to 11)
+ * @property day - [Int] corresponding to the year of the date (0 to 30)
+ */
 data class DatabaseDate(
     val year : Int,
     val month : Int,
