b658fcd219173ac9feaf403dabe37a4935ac8ce0
SavageBlink <drisskihal@gmail.com>
2022-06-16 15:32:08 +0200

    Modifying ZeroBudgetViewModel class to get subcategories

--- Sucelloz/app/src/main/java/com/sucelloztm/sucelloz/ui/zerobudget/ZeroBudgetViewModel.java
+++ Sucelloz/app/src/main/java/com/sucelloztm/sucelloz/ui/zerobudget/ZeroBudgetViewModel.java
@@ -1,13 +1,23 @@
 package com.sucelloztm.sucelloz.ui.zerobudget;
 
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class ZeroBudgetViewModel extends ViewModel {
-    private final MutableLiveData<String> mText;
+import com.sucelloztm.sucelloz.models.Categories;
+import com.sucelloztm.sucelloz.repositories.CategoriesRepository;
 
-    public ZeroBudgetViewModel() {
+public class ZeroBudgetViewModel extends AndroidViewModel {
+    private final MutableLiveData<String> mText;
+    private CategoriesRepository categoriesRepository;
+
+    public ZeroBudgetViewModel(Application application) {
+        super(application);
+        this.categoriesRepository = new CategoriesRepository(application);
+
         mText = new MutableLiveData<>();
         mText.setValue("This is Zero Budget fragment");
     }
@@ -15,5 +25,6 @@ public class ZeroBudgetViewModel extends ViewModel {
     public LiveData<String> getText() {
         return mText;
     }
+
 }
 
