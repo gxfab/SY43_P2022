5902a7dab7895d65d1fa937a632c7ba6624e8c9b
SulyvanDal <durand.sulyvan@gmail.com>
2022-06-16 19:35:51 +0200

    Patch background et ajout scrollView dans activity_recap

--- CookUTProject/app/src/main/java/com/example/cookutproject/Recap.java
+++ CookUTProject/app/src/main/java/com/example/cookutproject/Recap.java
@@ -23,17 +23,7 @@ public class Recap extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recap);
 
-        this.myLayout = findViewById(R.id.recapLayout);
 
-
-        TextView textRepas = new TextView(this);
-        textRepas.setText("Repas");
-        textRepas.setTextSize(50);
-        textRepas.setBackgroundColor(getResources().getColor(R.color.darkgreen_CookUT));
-        textRepas.setTextColor(getResources().getColor(R.color.white));
-
-        textRepas.setGravity(View.SCROLL_AXIS_HORIZONTAL);
-        myLayout.addView(textRepas);
     }
     public void changeActivityMain(View view){
         Intent intent = new Intent(this, MainActivity.class);
