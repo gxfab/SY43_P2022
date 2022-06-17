b8c15c484237d3df19de0df702af215131750126
Jnius0 <rjnius@gmail.com>
2022-06-17 13:22:36 +0200

    added the javadoc for the subcategories package

--- Sucelloz/app/src/main/java/com/sucelloztm/sucelloz/ui/subcategories/SubCategoriesViewModel.java
+++ Sucelloz/app/src/main/java/com/sucelloztm/sucelloz/ui/subcategories/SubCategoriesViewModel.java
@@ -11,16 +11,27 @@ import com.sucelloztm.sucelloz.repositories.SubCategoriesRepository;
 
 import java.util.List;
 
+/**
+ * view model for the subcategories
+ */
 public class SubCategoriesViewModel extends AndroidViewModel {
     private SubCategoriesRepository subCategoriesRepository;
 
     private LiveData<List<SubCategories>> currentSubCategories;
 
+    /**
+     * custom constructor
+     * @param application application
+     */
     public SubCategoriesViewModel(@NonNull Application application) {
         super(application);
         this.subCategoriesRepository= new SubCategoriesRepository(application);
     }
 
+    /**
+     * invokes the get subcategories query
+     * @return livedata of the subcategories
+     */
     public LiveData<List<SubCategories>> getSubCategories(){
         if (this.currentSubCategories == null){
             this.currentSubCategories = subCategoriesRepository.getSubCategoriesFromCurrentCategoryId();
@@ -28,10 +39,18 @@ public class SubCategoriesViewModel extends AndroidViewModel {
         return this.currentSubCategories;
     }
 
+    /**
+     * invokes the delete subcategory query
+     * @param subCategory subcategory
+     */
     public void deleteSubCategory(SubCategories subCategory){
         subCategoriesRepository.deleteSubCategory(subCategory);
     }
 
+    /**
+     * invokes the delete subcategory query
+     * @param subCategory subcategory
+     */
     public void updateSubCategory(SubCategories subCategory){
         subCategoriesRepository.updateSubCategory(subCategory);
     }
